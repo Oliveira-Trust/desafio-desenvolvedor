8fdb882b81b20d77819a61ce578addaea483fe3d
Jean Gomes <jeangp2006@gmail.com>
2020-02-22 20:09:16 -0300

    usando o conceito de QueryFilter para facilitar a filtragem de dados

---
+++ app/Filters/CustomerFilters.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class CustomerFilters extends QueryFilters
+{
+
+    /**
+     * Filter by cpf.
+     *
+     * @param  string $cpf
+     * @return Builder
+     */
+    public function cpf($cpf)
+    {
+        return $this->builder->where('cpf', $cpf);
+    }
+
+    /**
+     * Filter by name.
+     *
+     * @param  string $name
+     * @return Builder
+     */
+    public function name($name)
+    {
+        return $this->builder->where('name','like', '%'.$name.'%');
+    }
+
+    /**
+     * @param $email
+     * @return Builder
+     */
+    public function email($email)
+    {
+        return $this->builder->where('email', $email);
+    }
+
+}
