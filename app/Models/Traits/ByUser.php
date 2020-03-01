80e7dc0ea3dcf69a13c6c0d7bdd00346b1935f35
Heric Branco(INRI) <heric.branco@fidelize.com.br>
2020-03-01 18:13:05 -0300

    Trait responsavel por setar dados por usuário

--- app/Models/Traits/ByUser.php
+++ app/Models/Traits/ByUser.php
@@ -4,8 +4,18 @@ namespace App\Models\Traits;
 
 trait ByUser 
 {
+
     public function scopeByUser($query, $user)
     {
         return $query->where('user_id', $user);
+    }
+
+    public function save(array $options=[]) {
+        $this->user_id = auth()->user()->id;
+        parent::save($options);
+    }
+
+    public static function paginate($limit) {
+        return parent::byUser(auth()->user()->id)->paginate($limit);
     }
 }
