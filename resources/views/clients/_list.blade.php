9a365cd2bda213a228b1ddd09452279915e62b12
Heric Branco(INRI) <heric.branco@fidelize.com.br>
2020-03-02 01:28:35 -0300

    Utilizando o componente media para fazer as listagens

--- resources/views/clients/_list.blade.php
+++ resources/views/clients/_list.blade.php
@@ -1,14 +1,20 @@
-@foreach ($model as $client)
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">{{$client->name}}</h5>
-            <!--p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p-->
+<ul class="list-unstyled">
+@forelse ($model as $client)
+    <li class="media">
+        <!--img src="..." class="mr-3" alt="..."-->
+        <div class="media-body">
+            <h5 class="mt-0 mb-1">{{$client->name}}</h5>            
+        </div>
+        <div>
             <a href="{{URL::to('clients/'.$client->id.'/edit')}}" class="btn btn-primary"><i class="fas fa-edit"></i></a>
-            <form style="display:inline-block" action="{{action('ClientController@destroy', $client['id'])}}" method="post">
+            <form style="display:inline-block" action="{{action('ClientController@destroy', $client->id)}}" method="post">
                 @csrf
                 @method('DELETE')
                 <button class="btn btn-danger" type="submit"><i class="fa fa-trash"></i></button>
             </form>
         </div>
-    </div>
-@endforeach
+    </li>
+@empty
+    <div class="row justify-content-center mt-5">No Clients for User</div>
+@endforelse
+</ul>
