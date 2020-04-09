1145d7fbf5c6239c69558f2d152ef05b8dc829de
matheust45 <matheus.tavares45@gmail.com>
2020-04-09 19:07:43 -0300

    finishing migrations

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -11,21 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ElementsTableSeeder::class);
-        $this->call(RulesTableSeeder::class);
-        $this->call(PartsTableSeeder::class);
-        $this->call(BooksTableSeeder::class);
-        $this->call(TitlesTableSeeder::class);
-        $this->call(ChaptersTableSeeder::class);
-        $this->call(SectionsTableSeeder::class);
-        $this->call(SubsectionsTableSeeder::class);
-        $this->call(ArticlesTableSeeder::class);
-        $this->call(ParagraphsTableSeeder::class);
-        $this->call(IncisesTableSeeder::class);
-        $this->call(ItemsTableSeeder::class);
-        $this->call(LinesTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(UserRoleTableSeeder::class);
     }
 }
