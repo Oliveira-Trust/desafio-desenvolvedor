130376b9ab7367dee8c9270d8fd828beba87980e
matheust45 <matheus.tavares45@gmail.com>
2020-04-09 22:22:26 -0300

    Routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -27,4 +27,31 @@ Route::group(['middleware' => ['jwt.auth','api-header']], function () {
     Route::patch('user/auth-update', 'UserController@updateAuthenticated');
     Route::post('user/avatar-upload', 'UserController@avatarUpload');
     Route::resource('user', 'UserController')->only(['show']);
+
+    /**
+    * /client        => 'GET'    @ index()
+    * /client        => 'POST'   @ store()
+    * /client/{id}   => 'GET'    @ show()
+    * /client/{id}   => 'PATCH'  @ update()
+    * /client/{id}   => 'DELETE' @ destroy()
+    */
+    Route::resource('client', 'ClientController');
+
+    /**
+    * /product        => 'GET'    @ index()
+    * /product        => 'POST'   @ store()
+    * /product/{id}   => 'GET'    @ show()
+    * /product/{id}   => 'PATCH'  @ update()
+    * /product/{id}   => 'DELETE' @ destroy()
+    */
+    Route::resource('product', 'ProductController');
+
+    /**
+    * /order        => 'GET'    @ index()
+    * /order        => 'POST'   @ store()
+    * /order/{id}   => 'GET'    @ show()
+    * /order/{id}   => 'PATCH'  @ update()
+    * /order/{id}   => 'DELETE' @ destroy()
+    */
+    Route::resource('order', 'OrderController');
 });
