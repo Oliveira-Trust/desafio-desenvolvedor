74510804344f5357a8fed620f4ca4154f4622721
Wagner Lamoglia <wagnyero@gmail.com>
2020-06-13 17:32:43 -0300

    Seed de Status de pagamento

---
+++ database/seeds/OrderStatusSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class OrderStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('order_status')->insert([
+            'status' => "Aberto",
+        ]);
+
+        DB::table('order_status')->insert([
+            'status' => "Pago",
+        ]);
+        
+        DB::table('order_status')->insert([
+            'status' => "Cancelado",
+        ]);
+    }
+}
