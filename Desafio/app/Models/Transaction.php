e702614fa2f7149267395c4c56774c0679c8b168
jadersonmarc <jadersonmarc@gmail.com>
2020-08-19 10:32:34 -0300

    feat: created product model

---
+++ Desafio/app/Models/Transaction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $table = 'tansactions';
+
+    protected $fillable = ['id', 'status','client_id'];
+
+    public function clientes()
+    {
+        return $this->belongsTo(Cliente::class,'id', 'client_id');
+    }
+
+    public function itensTransactions()
+    {
+        return $this->hasMany(ItenTransaction::class,'id', 'client_id');
+    }
+}
