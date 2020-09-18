d47724d71fbca73c55f7fc2107e19259f193d940
pedro lucca <pedro.lucca@live.estacio.br>
2020-09-17 23:09:52 -0300

    estrutura básica produtos

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('products')->group(function (){
+    Route::get('', [ProductController::class,'index'])->name('products.index');
+    Route::get('create', [ProductController::class,'create'])->name("products.create");
+    Route::post('', [ProductController::class,'store'])->name("products.store");
+    Route::get('{product}', [ProductController::class,'show'])->name('product.show');
+    Route::get('{product}/edit', [ProductController::class,'edit'])->name('products.edit');
+    Route::patch('{product}', [ProductController::class,'update'])->name("product.update");
+    Route::delete('{product}', [ProductController::class,'destroy'])->name("product.destroy");
 });
