24c1a0b1677cfb9821ecdd29734ee0bd6e768e55
pedro lucca <pedro.lucca@live.estacio.br>
2020-09-19 19:49:38 -0300

    criando estrutura de product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,82 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Services\ProductService;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+    protected $productService;
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+   public function __construct(ProductService $productService)
+   {
+       $this->productService = $productService;
+   }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        //retorna view
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    public function index()
+    {
+        return $this->productService->all();
+    }
+
+    public function create()
+    {
+        //retorna view
+    }
+
+    //todo adicionar validaçãp de request
+    public function store(Request $request)
+    {
+        $this->productService->save($request);
+    }
+
     public function edit($id)
     {
-        //
+
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    //todo adicionar validaçãp de request
     public function update(Request $request, $id)
     {
-        //
+        return $this->productService->update($id, $request);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $destroidItem = $this->productService->destroy($id);
+
+        if (!$destroidItem) return "item não encontrado";
+
+        return "item deleteado";
     }
 }
---
+++ app/Services/ProductService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\ProductRepository;
+
+class ProductService
+{
+    protected $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function all()
+    {
+        return $this->productRepository->all();
+    }
+
+    public function save(array $attributes)
+    {
+        return $this->productRepository->save($attributes);
+    }
+
+    public function update(array $attributes, int $id)
+    {
+        return $this->productRepository->update($id, $attributes);
+    }
+
+    public function destroy(int $id)
+    {
+        return $this->productRepository->destroy($id);
+    }
+}
