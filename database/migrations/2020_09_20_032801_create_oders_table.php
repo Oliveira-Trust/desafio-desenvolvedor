5f2f8b55d146fac5820335235ba450d5ca5c6dac
pedro lucca <pedro.lucca@live.estacio.br>
2020-09-20 01:40:05 -0300

    criando estrutura de pedidos

---
+++ database/migrations/2020_09_20_032801_create_oders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('client_id');
+            $table->timestamps();
+            $table->foreign('client_id')->references('id')->on('clients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oders');
+    }
+}
