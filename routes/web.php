9f3636ec037f3b7b19eccf3ea8add58319f1c3a3
pedro lucca <pedro.lucca@live.estacio.br>
2020-09-19 21:06:11 -0300

    resouce routes

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,10 @@ Route::get('/', function () {
 
 Route::prefix('products')->group(function (){
     Route::get('', [ProductController::class,'index'])->name('products.index');
-    Route::get('create', [ProductController::class,'create'])->name("products.create");
+    Route::post('create', [ProductController::class,'create'])->name("products.create");
     Route::post('', [ProductController::class,'store'])->name("products.store");
     Route::get('{product}', [ProductController::class,'show'])->name('product.show');
     Route::get('{product}/edit', [ProductController::class,'edit'])->name('products.edit');
-    Route::patch('{product}', [ProductController::class,'update'])->name("product.update");
-    Route::delete('{product}', [ProductController::class,'destroy'])->name("product.destroy");
+    Route::post('/update/{product}', [ProductController::class,'update'])->name("product.update");
+    Route::post('/destroy/{product}', [ProductController::class,'destroy'])->name("product.destroy");
 });
