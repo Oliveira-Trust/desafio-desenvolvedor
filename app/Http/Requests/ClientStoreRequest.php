fe3504cce229d87eaa3a7982bb59e352d4c2a596
pedro lucca <pedro.lucca@live.estacio.br>
2020-09-20 21:28:17 -0300

    feat: validação de request ao salvar um pedido

---
+++ app/Http/Requests/ClientStoreRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ClientStoreRequest extends FormRequest
+{
+    /**
+     * Disable validator redirect back to use in API
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:50',
+            'email' => 'required|email',
+        ];
+    }
+}
---
+++ app/Http/Requests/OrderStoreRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class OrderStoreRequest extends FormRequest
+{
+    /**
+     * Disable validator redirect back to use in API
+     *
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator) {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'client_id' => 'required',
+            'status' => 'required|in:pending',
+            'quantity' => 'required|numeric',
+            'price' => 'required|numeric',
+            'product_id' => 'required'
+        ];
+    }
+}
