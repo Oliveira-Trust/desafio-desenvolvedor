3b5ad0570613121a85d52ec4fa220d65d938cbf4
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-06-30 21:24:24 -0300

    instalando o vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .sourceMaps().vue({version : 3});
