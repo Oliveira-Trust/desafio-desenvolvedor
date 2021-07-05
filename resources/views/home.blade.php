4fa8dc19a8c454d185bc7b656c5dc598fe1cd93c
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-07-05 01:51:34 -0300

    Oliveira Trust Challenge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,15 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header text-center"><h4>{{ __('Dashboard') }}</h4></div>
 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    @endif                    
+                    <h2 class="text-center text-bold">Oliveira Trust Challenge</h2>
                 </div>
             </div>
         </div>
