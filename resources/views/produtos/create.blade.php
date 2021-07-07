e90058fd3784bb8760e21005065bcf2af08636a9
Walder Silva <walderalexandre@gmail.com>
2021-07-07 15:25:21 -0300

    ajuste no formulário de produto

--- resources/views/produtos/create.blade.php
+++ resources/views/produtos/create.blade.php
@@ -1,12 +1,25 @@
-<html>
+@extends('layouts.app')
 
-<form method="POST" action="{{route('salvar_produto')}}">
-  @csrf  
-Descrição: <input type="text" name="descricao"><br>
-Valor: <input type="text" name="valor"><br>
-Quantidade: <input type="text" name="quantidade"><br>
-<button>Salvar</button>
+@section('title')
+Produto     
+@endsection
 
+@section('content')
+<form class="row g-3" method="POST" action="{{route('salvar_produto')}}">
+    @csrf  
+    <div class="mb-3">
+        <label for="descricao" class="form-label">Descricao</label>
+        <input type="text" class="form-control" id="descricao" name="descricao">
+    </div>
+    <div class="mb-3">
+        <label for="valor" class="form-label">Valor</label>
+        <input type="text" class="form-control" id="valor" name="valor">
+    </div>
+    <div class="mb-3 form-check">
+        <label for="quantidade" class="form-label">Quantidade</label>
+        <input type="number" step="1" class="form-control" id="quantidade" name="quantidade">
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Salvar</button>
 </form>
-
-</html>
+@endsection
