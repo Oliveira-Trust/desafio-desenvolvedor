111cb5b7d2be034e570e91fa9873bf3b78e00e23
Walder Silva <walderalexandre@gmail.com>
2021-07-07 10:34:23 -0300

    criando o formulário de cadastro de produto

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    //Tela para criação de produto
+    public function create()
+    {
+        return view('produtos.create');
+    }
+}
---
+++ resources/views/produtos/create.blade.php
@@ -0,0 +1,11 @@
+<html>
+
+<form action="">
+Descrição: <input type="text" name="descricao"><br>
+Valor: <input type="text" name="valor"><br>
+Quantidade: <input type="text" name="quantidade"><br>
+<button>Salvar</button>
+
+</form>
+
+</html>
