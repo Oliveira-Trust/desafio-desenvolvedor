db82a8f17c6f5c5acef222d1657d9e547f2da5ed
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-07-17 16:19:30 -0300

    troca do nome da aplicação

--- config/app.php
+++ config/app.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => "Desafio Oliveira Trust",
+    //'name' => env('APP_NAME', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
