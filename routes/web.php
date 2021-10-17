f64d29c2b8e3120191e61d1c555c37eb75572d72
dsleite360 <diegodesouzaleite@hotmail.com>
2021-10-17 16:57:54 -0300

    Instalação do jetstream

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use App\Http\Controllers\Awesome\{AvaliableController, BuyController};
 
 Route::get('/', [AvaliableController::class, 'index']);
 Route::post('/buy', [BuyController::class, 'buy'])->name('buy');
+
+
+
+Route::middleware(['auth:sanctum', 'verified'])->prefix('dashboard')->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::get('/taxas', function () {
+        return view('taxes');
+    })->name('taxes');
+});
