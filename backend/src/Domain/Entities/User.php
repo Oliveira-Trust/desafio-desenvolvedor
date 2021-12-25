819eb877e679e1a9269bd5211bd26047e88b3184
Daniel Meireles <daniel@inovadora.com.br>
2021-12-25 07:59:10 -0300

    Organizando pastas para Domain

---
+++ backend/src/Domain/Entities/User.php
@@ -0,0 +1,133 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repositories\UserRepository")
+ * @ORM\Table(name="users")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $username;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $password;
+      /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name.
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set username.
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username.
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password.
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+        return $this;
+    }
+    /**
+     * Get password.
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    /**
+     * 
+     *
+     * @return bool
+     */
+    public function validatePassword(string $password, string $has): bool
+    {
+        return password_verify($password, $has);
+    }
+    public function toArray()
+    {
+        $array = [];
+        $keys = array_keys(get_class_vars(get_class($this)));
+        foreach($keys as $key ){
+            if($key == 'password') {
+                continue;
+            } 
+            $method = 'get'.str_replace(" ", '', ucwords(str_replace('_', ' ', $key))) ;
+            $array[$key] = $this->$method();
+        }
+        return $array;
+    }    
+}
---
+++ backend/src/Domain/Repositories/UserRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Repositories;
+
+use App\Entities\User;
+use App\Helpers\EntityManagerFactory;
+
+class UserRepository extends \Doctrine\ORM\EntityRepository
+{
+    protected $entityManager;
+    public function __construct(EntityManagerFactory $entityManagerFactory)
+    {
+        $this->entityManager = $entityManagerFactory->getEntityManager();
+    }
+    public function getById(int $id)
+    {
+        $query = $this->entityManager->createQuery('SELECT u FROM App\Entities\User u WHERE u.id = :paramid');
+        $query->setParameter('paramid', $id);
+        return $query->getResult()[0] ?? false;
+    }
+    public function delete(User $user):void
+    {
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+    }
+    public function getAll(): array
+    {
+        $query = $this->entityManager->createQuery('SELECT u FROM App\Entities\User u');
+        $results = $query->getResult();
+        $response = [];
+        foreach ($results as $user) {
+            array_push($response, $user->toArray());
+        }
+        return $response;
+    }
+    public function getByUsername(string $username)
+    {
+        $query = $this->entityManager->createQuery('SELECT u FROM App\Entities\User u WHERE u.username = :username');
+        $query->setParameter('username', $username);
+        return $query->getResult()[0] ?? false;
+    }
+    public function save(User $user): User
+    {
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        return $user;
+    }
+}
