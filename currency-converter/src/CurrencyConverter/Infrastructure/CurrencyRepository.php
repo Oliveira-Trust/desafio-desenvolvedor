093e089c9cbe74e6f8a743b23c3db934d65cfc24
Tiago Oliveira de Farias <tiago.farias.poa@gmail.com>
2021-12-26 19:27:18 -0300

    add - find availables combinations

--- currency-converter/src/CurrencyConverter/Infrastructure/CurrencyRepository.php
+++ currency-converter/src/CurrencyConverter/Infrastructure/CurrencyRepository.php
@@ -4,6 +4,7 @@ namespace CurrencyConverter\Infrastructure;
 
 
 use CurrencyConverter\Domain\Currency\Repositories\CurrencyInterface;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Collection;
 
 /**
@@ -14,6 +15,29 @@ class CurrencyRepository implements CurrencyInterface
 {
     public function findAvailablesCombinations(): Collection
     {
-        dd(__METHOD__);
+        $collection = new Collection();
+
+        try {
+            $response = Http::accept('application/xml')->get('https://economia.awesomeapi.com.br/xml/available')
+                                                                  ->throw()
+                                                                  ->body();
+
+            $xmlData = simplexml_load_string($response);
+            $jsonData = json_encode($xmlData );
+            $data = json_decode($jsonData,TRUE);
+
+            $filteredData = array_filter(
+                $data,
+                fn ($key) => substr($key, 0, strlen('BRL-')) === 'BRL-',
+                ARRAY_FILTER_USE_KEY
+            );
+
+            $collection = collect($filteredData);
+        }catch(\Exception $e)
+        {
+            throw new $e;
+        }
+
+        return $collection;
     }
 }
