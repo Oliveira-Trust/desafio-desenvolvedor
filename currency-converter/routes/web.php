31bc022531c882e59e7f98b691e919317be45770
Tiago Oliveira de Farias <tiago.farias.poa@gmail.com>
2021-12-26 14:01:31 -0300

    fix - auth config

--- currency-converter/routes/web.php
+++ currency-converter/routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('home','home')->middleware('auth');
