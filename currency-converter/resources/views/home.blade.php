086a90ddf4c853925378173ac7852e229b06f1be
Tiago Oliveira de Farias <tiago.farias.poa@gmail.com>
2021-12-26 18:04:24 -0300

    add - form

--- currency-converter/resources/views/home.blade.php
+++ currency-converter/resources/views/home.blade.php
@@ -3,18 +3,55 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header"><h3 class="card-title"></h3></div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <form name="" action="" method="POST">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    <label for="origin_currency">Moeda de origem:</label>
+                                    <input style="cursor:not-allowed" type="text" readonly="readonly" class="form-control" id="origin_currency" value="BRL"title="Real Brasileiro" placeholder="">
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    <label for="destiny_currency">Moeda de destino:</label>
+                                    <select id="destiny_currency" class="form-select" style="width: 100%">
+                                        <option></option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    <label for="value_for_conversion">Valor para conversão:</label>
+                                    <input type="text" class="form-control" id="value_for_conversion" title="Valor para conversão" placeholder="">
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="form-group">
+                                    <label for="payment method">Forma de pagamento:</label>
+                                    <select class="form-select" id="payment method" style="width: 100%">
+                                        <option value="1">Boleto</option>
+                                        <option value="2">Cartão de Crédito</option>
+                                    </select>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="row">
+                            <div class="col-md-12 d-grid gap-2 mt-3">
+                                <button id="btn-search-items" type="submit" class="btn btn-primary btn-block">
+                                    <i class="fas fa-search"></i>&nbsp;Converter
+                                </button>
+                            </div>
+                        </div>
+                        @error('manufactureCode')
+                        <div class="error-label">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </form>
                 </div>
             </div>
         </div>
