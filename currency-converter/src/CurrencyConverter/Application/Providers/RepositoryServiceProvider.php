79806788bf7d8d2d5dee254ea0ecb5ff6029916b
Tiago Oliveira de Farias <tiago.farias.poa@gmail.com>
2021-12-26 18:52:00 -0300

    add - RepositoryServiceProvider

---
+++ currency-converter/src/CurrencyConverter/Application/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CurrencyConverter\Domain\Currency\Services;
+
+/**
+ * Class RepositoryServiceProvider
+ * @package CurrencyConverter\Domain\Currency\Services
+ * @author Tiago O. de Farias <tiago.farias.poa@gmail.com>
+ */
+class RepositoryServiceProvider
+{
+
+}
