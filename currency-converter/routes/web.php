9fa46b639187ea180992efcad5dfdf6fab5e9433
Tiago Oliveira de Farias <tiago.farias.poa@gmail.com>
2021-12-26 17:21:57 -0300

    add - dedicated structure

--- currency-converter/routes/web.php
+++ currency-converter/routes/web.php
@@ -16,9 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::view('home','home')->middleware('auth');
---
+++ currency-converter/src/CurrencyConverter/Application/Routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('home','home')->middleware('auth');
