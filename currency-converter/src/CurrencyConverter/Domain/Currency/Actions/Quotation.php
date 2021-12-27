57373e8664923056cb3751c09f33fa67276866c2
Tiago Oliveira de Farias <tiago.farias.poa@gmail.com>
2021-12-27 08:31:57 -0300

    fix - action: quotation

--- currency-converter/src/CurrencyConverter/Domain/Currency/Actions/Quotation.php
+++ currency-converter/src/CurrencyConverter/Domain/Currency/Actions/Quotation.php
@@ -2,7 +2,8 @@
 
 namespace CurrencyConverter\Domain\Currency\Actions;
 
-use \CurrencyConverter\Domain\Currency\DTOs\FormData as ConvertDTO;
+use CurrencyConverter\Domain\Currency\DTOs\FormData as FormDataDTO;
+use CurrencyConverter\Domain\Currency\DTOs\ListData;
 use CurrencyConverter\Domain\Currency\Services\CurrencyService;
 
 /**
@@ -12,9 +13,57 @@ use CurrencyConverter\Domain\Currency\Services\CurrencyService;
  */
 class Quotation
 {
-    public function __invoke(ConvertDTO $dto)
+    const BOLETO_RATE = 0.0145;
+    const CREDIT_CARD_RATE = 0.0763;
+
+    public function __invoke(FormDataDTO $dto)
     {
         $service = app(CurrencyService::class);
-        return $service->getQuotation($dto);
+        $quotationData = $service->getQuotation($dto);
+
+        ListData::$originCurrency = 'BRL';
+        ListData::$destinyCurrency = $dto::$destinyCurrency;
+        ListData::$valueConversion = $dto::$valueConversion;
+        ListData::$paymentMethod = $dto::$paymentMethod == 1 ? 'Boleto' : 'Cartão de Crédito';
+        ListData::$destinationCurrencyValueForConversion = $quotationData['bid'];
+        ListData::$valuePurchasesInDestinationCurrency = $this->calculateValuePurchasesInDestinationCurrency($dto::$paymentMethod, $dto::$valueConversion);
+        ListData::$paymentRate = $this->getPaymentRate($dto::$paymentMethod, $dto::$valueConversion);
+    }
+
+    /**
+     * @param int $paymentMethod
+     * @param string $valueConversion
+     * @return float|int
+     */
+    private function getPaymentRate(int $paymentMethod ,string $valueConversion)
+    {
+        if( $paymentMethod != 1 && $paymentMethod != 2 ){
+            throw new \DomainException('Invalid payment method!');
+        }
+
+        if( $paymentMethod == 1 ){
+            return $valueConversion * (1.45/100);
+        }
+
+        return $valueConversion * (7.63/100);
+    }
+
+    /**
+     * @param int $paymentMethod
+     * @param string $valueConversion
+     * @return float|int
+     */
+    private function getConversionRate(int $paymentMethod ,string $valueConversion) : float
+    {
+        if( $valueConversion < 3000 )
+        {
+            return $valueConversion * (2/100);
+        }
+        return $valueConversion * (1/100);
+    }
+
+    private function calculateValuePurchasesInDestinationCurrency(int $paymentMethod ,string $valueConversion) : float
+    {
+        return $valueConversion - ($this->getPaymentRate($paymentMethod, $valueConversion) + $this->getConversionRate($paymentMethod, $valueConversion));
     }
 }
---
+++ currency-converter/src/CurrencyConverter/Domain/Currency/DTOs/ListData.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CurrencyConverter\Domain\Currency\DTOs;
+
+/**
+ * Class Convert
+ * @package CurrencyConverter\Domain\Currency\DTOs
+ * @author Tiago O. de Farias <tiago.farias.poa@gmail.com>
+ */
+class ListData
+{
+    public static string $destinyCurrency;
+    public static string $valueConversion;
+    public static string $paymentMethod;
+    public static string $destinationCurrencyValueForConversion;
+    public static string $destinationCurrencyValue;
+    public static string $paymentRate;
+    public static string $conversionRate;
+}
