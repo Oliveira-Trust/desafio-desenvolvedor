8d090553a53a281b22acc9dfc6eb2baa1cab2389
Tiago Oliveira de Farias <tiago.farias.poa@gmail.com>
2021-12-27 16:08:56 -0300

    fix - layout quotation history

--- currency-converter/resources/views/includes/history.blade.php
+++ currency-converter/resources/views/includes/history.blade.php
@@ -9,46 +9,61 @@
                     <div class="card mt-1">
                         <div class="card-body">
 
-                            <table class="table table-striped table-hover table-sm align-middle">
-                                <tbody>
-                                <tr>
-                                    <td>Moeda de origem</td>
-                                    <td><span>{{$quotationData['origin_currency']}}</span></td>
-                                </tr>
-                                <tr>
-                                    <td>Moeda de destino</td>
-                                    <td><span>{{$quotationData['destiny_currency']}}</span></td>
-                                </tr>
-                                <tr>
-                                    <td>Valor para conversão</td>
-                                    <td><span>R$ {{$quotationData['value_conversion']}}</span></td>
-                                </tr>
-                                <tr>
-                                    <td>Forma de pagamento</td>
-                                    <td><span>{{$quotationData['payment_method']}}</span></td>
-                                </tr>
-                                <tr>
-                                    <td>Valor da "Moeda de destino" usado para conversão</td>
-                                    <td><span>R$ {{$quotationData['destination_currency_value_for_conversion']}}</span></td>
-                                </tr>
-                                <tr>
-                                    <td>Valor comprado em "Moeda de destino"</td>
-                                    <td><span>R$ {{$quotationData['destination_currency_value_for_conversion']}}</span></td>
-                                </tr>
-                                <tr>
-                                    <td>Taxa de pagamento</td>
-                                    <td><span>R$ {{$quotationData['payment_rate']}}</span></td>
-                                </tr>
-                                <tr>
-                                    <td>Taxa de conversão</td>
-                                    <td><span>R$ {{$quotationData['conversion_rate']}}</span></td>
-                                </tr>
-                                <tr>
-                                    <td>Valor utilizado para conversão descontando as taxas"</td>
-                                    <td><span>R$ {{$quotationData['value_purchases_in_destination_currency']}}</span></td>
-                                </tr>
-                                </tbody>
-                            </table>
+                            @foreach($quotationData as $key => $item)
+                                <div class="accordion mt-1" id="">
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="headingOne">
+                                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{$key}}" aria-expanded="true" aria-controls="collapseOne">
+                                                Data da cotação: {{$item->created_at->format('Y-m-d H:i')}}
+                                            </button>
+                                        </h2>
+                                        <div id="collapse{{$key}}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                            <div class="accordion-body">
+                                                <table class="table table-striped table-hover table-sm align-middle">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td>Moeda de origem</td>
+                                                        <td><span>BRL</span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Moeda de destino</td>
+                                                        <td><span>{{$item->destiny_currency}}</span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Valor para conversão</td>
+                                                        <td><span>R$ {{$item->value_for_conversion}}</span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Forma de pagamento</td>
+                                                        <td><span>{{$item->payment_method}}</span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Valor da "Moeda de destino" usado para conversão</td>
+                                                        <td><span>R$ </span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Valor comprado em "Moeda de destino"</td>
+                                                        <td><span>R$ </span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Taxa de pagamento</td>
+                                                        <td><span>R$ </span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Taxa de conversão</td>
+                                                        <td><span>R$ </span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Valor utilizado para conversão descontando as taxas"</td>
+                                                        <td><span>R$ </span></td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
 
                         </div>
                     </div>
