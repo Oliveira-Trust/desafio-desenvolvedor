47285a68d8397877f707573338f113ca1ea0e851
Daniel Meireles <daniel@inovadora.com.br>
2021-12-28 07:31:48 -0300

    Iniciando troca de framework para solucionar problema nas rotas.

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,7 +4,6 @@ use App\Helpers\EntityManagerFactory;
 require_once __DIR__. '/../config/config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$emFactory = new EntityManagerFactory();
 // Define app routes
 require_once __DIR__ .'/../routes/routes.php';
 // Run app
