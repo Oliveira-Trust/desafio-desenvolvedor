9a8ba0268ba0ad0c9d73702a8205fc71d9ed7d33
Moisés Fausto <moises_system@yahoo.com.br>
2022-01-09 13:37:36 -0300

    Add layout response result

--- desafioDesenvolvedor/resources/views/home.blade.php
+++ desafioDesenvolvedor/resources/views/home.blade.php
@@ -80,6 +80,23 @@
                     </form>
                 </div>
             </div>
+
+            <div class="card mt-5 d-none">
+                <div class="card-header">
+                  Resultado da Conversão
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Moeda de origem: BRL</li>
+                  <li class="list-group-item">Moeda de destino: USD</li>
+                  <li class="list-group-item">Valor para conversão: R$ 5.000,00</li>
+                  <li class="list-group-item">Forma de pagamento: Boleto</li>
+                  <li class="list-group-item">Valor da "Moeda de destino" usado para conversão: $ 5,30</li>
+                  <li class="list-group-item">Valor comprado em "Moeda de destino": $ 920,18 (taxas aplicadas no valor de compra diminuindo no valor total de conversão)</li>
+                  <li class="list-group-item">Taxa de pagamento: R$ 72,50</li>
+                  <li class="list-group-item">Taxa de conversão: R$ 50,00</li>
+                  <li class="list-group-item">Valor utilizado para conversão descontando as taxas: R$ 4.877,50</li>
+                </ul>
+            </div>
         </div>
     </div>
 </div>
