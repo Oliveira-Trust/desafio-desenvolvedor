da6bbad2d3246d4ba02623a57d3ab8ef26e251f7
Moisés Fausto <moises_system@yahoo.com.br>
2022-01-08 23:07:39 -0300

    inclued script js

--- desafioDesenvolvedor/resources/views/layouts/app.blade.php
+++ desafioDesenvolvedor/resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/script.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
--- desafioDesenvolvedor/webpack.mix.js
+++ desafioDesenvolvedor/webpack.mix.js
@@ -12,4 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/script.js', 'public/js')
+
+
     .sass('resources/sass/app.scss', 'public/css');
