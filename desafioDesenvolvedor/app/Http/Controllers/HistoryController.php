8ee24c0d03b1abf837fbffa1ae77882bdda9c044
Moises Fausto <moises.fausto@klinisaude.com.br>
2022-01-10 18:52:00 -0300

    Update Authentication to History Page

--- desafioDesenvolvedor/app/Http/Controllers/HistoryController.php
+++ desafioDesenvolvedor/app/Http/Controllers/HistoryController.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Facades\Auth;
 
 class HistoryController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $user = Auth::user();
