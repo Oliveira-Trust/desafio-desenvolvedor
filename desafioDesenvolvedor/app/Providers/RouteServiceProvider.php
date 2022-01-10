a46ee67c9d093ae1e539962952622a48c9c78fb2
Moises Fausto <moises.fausto@klinisaude.com.br>
2022-01-10 18:39:16 -0300

    Alter const HOME to /

--- desafioDesenvolvedor/app/Providers/RouteServiceProvider.php
+++ desafioDesenvolvedor/app/Providers/RouteServiceProvider.php
@@ -21,7 +21,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    // public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
