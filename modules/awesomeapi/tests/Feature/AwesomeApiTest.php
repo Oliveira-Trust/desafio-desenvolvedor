f2959438a7d01cfbf13a819360c3431942691ac5
vitor-melo <vitorpmelo20@gmail.com>
2022-01-22 12:49:42 -0300

    create test case, then refactor

--- modules/awesomeapi/tests/Feature/AwesomeApiTest.php
+++ modules/awesomeapi/tests/Feature/AwesomeApiTest.php
@@ -1,6 +1,16 @@
 <?php
 
-class AwesomeApiTest
-{
+declare(strict_types=1);
 
+namespace AwesomeApi\Tests\Feature;
+
+use Tests\TestCase;
+
+class AwesomeApiTest extends TestCase
+{
+    public function test_should_return_an_json(): void
+    {
+        $response = $this->json('GET', '/list-currencies');
+        dd($response->json());
+    }
 }
