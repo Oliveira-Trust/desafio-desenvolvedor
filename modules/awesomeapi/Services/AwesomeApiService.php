8dd87dabaf7267fc98eb4724911d30409c635452
vitor-melo <vitorpmelo20@gmail.com>
2022-01-22 12:33:26 -0300

    create service layer

--- modules/awesomeapi/Services/AwesomeApiService.php
+++ modules/awesomeapi/Services/AwesomeApiService.php
@@ -1,6 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
+namespace AwesomeApi\Services;
+
+use AwesomeApi\Connection\HttpConnection;
+use Illuminate\Http\Client\Response;
+
 class AwesomeApiService
 {
+    private HttpConnection $httpConnection;
 
+    public function __construct(HttpConnection $httpConnection)
+    {
+        $this->httpConnection = $httpConnection;
+    }
+
+    public function currenciesAvailable(): Response
+    {
+        return $this->httpConnection->currenciesAvailable();
+    }
 }
