d73a7db7ef5e88f0f2589ce49dddffba4343a267
vitor-melo <vitorpmelo20@gmail.com>
2022-01-22 12:22:51 -0300

    add AwesomeApiServiceProvider class in app file

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        AwesomeApi\Providers\AwesomeApiServiceProvider::class,
 
         /*
          * Application Service Providers...
