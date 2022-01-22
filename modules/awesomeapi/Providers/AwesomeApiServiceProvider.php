6ac12b91cdbc3354dd33cc42841b50acc6c6dd71
vitor-melo <vitorpmelo20@gmail.com>
2022-01-22 12:31:44 -0300

    create service provider, and change namespace

--- modules/awesomeapi/Providers/AwesomeApiServiceProvider.php
+++ modules/awesomeapi/Providers/AwesomeApiServiceProvider.php
@@ -1,6 +1,26 @@
 <?php
 
-class AwesomeApiServiceProvider
-{
+declare(strict_types=1);
 
+namespace AwesomeApi\Providers;
+
+use AwesomeApi\Connection\ClientJsonAdapter;
+use AwesomeApi\Connection\HttpConnection;
+use AwesomeApi\Tests\ServerMock\AwesomeApiMock;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\ServiceProvider;
+
+class AwesomeApiServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/awesomeApi.php');
+
+        $this->app->bind(HttpConnection::class, ClientJsonAdapter::class);
+
+        Http::fake(function(Request $request) {
+            return (new AwesomeApiMock())->handle($request);
+        });
+    }
 }
