a9c4a5a02c769fc3d54ff86c90937fe114ea74c4
vitor-melo <vitorpmelo20@gmail.com>
2022-01-22 00:43:44 -0300

    create group of routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->group(function(): void {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
