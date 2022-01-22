1718d9eddf0268bb7f835724f129e00f439dc7cc
vitor-melo <vitorpmelo20@gmail.com>
2022-01-22 12:32:32 -0300

    create controller and enums of routes

--- modules/awesomeapi/Connection/AwesomeRoutes.php
+++ modules/awesomeapi/Connection/AwesomeRoutes.php
@@ -1,6 +1,10 @@
 <?php
 
-class AwesomeRoutes
-{
+declare(strict_types=1);
 
+namespace AwesomeApi\Connection;
+
+final class AwesomeRoutes
+{
+    public const FIRST_ROUTE = '/json/available';
 }
--- modules/awesomeapi/Controllers/AwesomeApiController.php
+++ modules/awesomeapi/Controllers/AwesomeApiController.php
@@ -1,6 +1,24 @@
 <?php
 
-class AwesomeApiController
-{
+declare(strict_types=1);
 
+namespace AwesomeApi\Controllers;
+
+use App\Http\Controllers\Controller;
+use AwesomeApi\Services\AwesomeApiService;
+use Illuminate\Http\Client\Response;
+
+class AwesomeApiController extends Controller
+{
+    private AwesomeApiService $awesomeApiService;
+
+    public function __construct(AwesomeApiService $awesomeApiService)
+    {
+        $this->awesomeApiService = $awesomeApiService;
+    }
+
+    public function listAvailableCurrencies(): Response
+    {
+        return $this->awesomeApiService->currenciesAvailable();
+    }
 }
