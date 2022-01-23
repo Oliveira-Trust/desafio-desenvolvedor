acc727974c68cadcf7e121e9269fb12d529b0ec8
vitor-melo <vitorpmelo20@gmail.com>
2022-01-22 22:19:07 -0300

    refactoring in class AdapterCurrencyCollection

--- modules/awesomeapi/Adapters/AdapterCurrencyCollection.php
+++ modules/awesomeapi/Adapters/AdapterCurrencyCollection.php
@@ -2,10 +2,37 @@
 
 declare(strict_types=1);
 
+namespace AwesomeApi\Adapters;
+
 class AdapterCurrencyCollection
 {
-    /** @param mixed[] $array */
-    public function __construct(array $array)
+    private array $currencies;
+    private array $currenciesAdapted;
+    private const CURRENCY_DEFAULT = 'BRL';
+
+    /** @param string[] $array */
+    public function __construct(array $currencies)
     {
+        $this->currencies = $currencies;
+    }
+
+    /** @return string[] */
+    public function getCurrencies(): array
+    {
+        foreach ($this->currencies as $prefix => $label) {
+            $this->AdaptCombinationOnlyBrl($prefix, $label);
+        }
+
+        return $this->currenciesAdapted;
+    }
+
+    private function AdaptCombinationOnlyBrl($prefix, $label): void
+    {
+        if (str_contains($prefix, '-' . self::CURRENCY_DEFAULT)) {
+            $this->currenciesAdapted[] = [
+                'prefix' => $prefix,
+                'label' => explode('/', $label)[0] ?? ''
+            ];
+        }
     }
 }
