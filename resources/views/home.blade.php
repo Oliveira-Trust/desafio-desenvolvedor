1ee502502b79c41fc0c43e8000d904f5bae5c43a
vitor-melo <vitorpmelo20@gmail.com>
2022-01-23 00:32:58 -0300

    increment new fields in form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,14 +9,15 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-6">
+                            <label>Converter de:</label>
                             <select id="origin-currency"
                                     data-show-content="true"
-                                    class="form-control bg-primary text-white"
-                            >
+                                    class="form-control bg-primary text-white">
                                 <option>Real Brasileiro (BRL)</option>
                             </select>
                         </div>
                         <div class="col-6">
+                            <label>Insira o valor (R$):</label>
                             <input id="money" type="text" class="form-control"
                                    name="money"
                                    placeholder="R$10,00"
@@ -25,8 +26,9 @@
                             >
                         </div>
                     </div>
-                    <div class="row mt-2">
+                    <div class="row mt-3">
                         <div class="col-12">
+                            <label>Para:</label>
                             <select id="destiny-currency"
                                     data-show-content="true"
                                     class="form-control bg-primary text-white">
@@ -35,11 +37,35 @@
                                     <option
                                         value="{{ $d['prefix'] }}"
                                     >
-                                        {{ $d['label'] }}({{ str_replace('-BRL', '', $d['prefix']) }})
+                                        {{ $d['label'] }} ({{ str_replace('-BRL', '', $d['prefix']) }})
                                     </option>
                                 @endforeach
                             </select>
                         </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-12 text-left text-justify d-flex justify-content-between align-items-baseline">
+                            <label>Forma de Pagamento: </label>
+                            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                <label class="btn btn-primary text-center">
+                                    <input type="radio" name="credit-card"
+                                           id="credit-card" autocomplete="off">Cartão de Crédito
+                                </label>
+
+                                <label class="btn btn-primary text-center">
+                                    <input type="radio" name="bank-invoice"
+                                           id="bank-invoice" autocomplete="off">Boleto
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-4 justify-content-center">
+                        <button class="btn btn-primary btn-block mr-2 ml-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                            </svg>
+                            Gerar Cotação
+                        </button>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand text-bold" href="{{ url('/') }}">
                     Oliveira Trust
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
