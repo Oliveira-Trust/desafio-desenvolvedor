c35b003405108d6d2e0b37d5a6ef0128dc545116
vitor-melo <vitorpmelo20@gmail.com>
2022-01-23 16:09:56 -0300

    some adjusments, and dependency injection

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,23 +4,34 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\QuoteGenerateFormRequest;
+use App\Services\PaymentService;
 use AwesomeApi\Services\AwesomeApiService;
 use Illuminate\Contracts\Support\Renderable;
 
 class HomeController extends Controller
 {
     private AwesomeApiService $awesomeApiService;
+    private PaymentService $paymentService;
 
-    public function __construct(AwesomeApiService $awesomeApiService)
+    public function __construct(AwesomeApiService $awesomeApiService, PaymentService $paymentService)
     {
         $this->awesomeApiService = $awesomeApiService;
+        $this->paymentService = $paymentService;
     }
 
     public function index(): Renderable
     {
-        $currencies = $this->awesomeApiService->currenciesAvailable();
+        return view('home')
+            ->with(
+                'data',
+                $this->awesomeApiService->currenciesAvailable()
+            );
+    }
 
-        return view('home')->with('data', $currencies);
+    public function generateQuote(QuoteGenerateFormRequest $request)
+    {
+        $this->paymentService->quoteGenerate($request->validated());
     }
 
     public function quoteHistory(): Renderable
