71e8dac7a4678087472ad2f5a99b5b524021e9b9
vitor-melo <vitorpmelo20@gmail.com>
2022-01-22 22:18:33 -0300

    add variable  in blade

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,27 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use AwesomeApi\Services\AwesomeApiService;
 use Illuminate\Contracts\Support\Renderable;
 
 class HomeController extends Controller
 {
+    private AwesomeApiService $awesomeApiService;
+
+    public function __construct(AwesomeApiService $awesomeApiService)
+    {
+        $this->awesomeApiService = $awesomeApiService;
+    }
+
     public function index(): Renderable
     {
-        return view('home');
+        $currencies = $this->awesomeApiService->currenciesAvailable();
+
+        return view('home')->with('data', $currencies);
+    }
+
+    public function quoteHistory(): Renderable
+    {
+        return view('quote-history');
     }
 }
