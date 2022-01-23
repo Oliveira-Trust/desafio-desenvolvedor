d8cbaea6753040333ad545de8696a93903681fd3
vitor-melo <vitorpmelo20@gmail.com>
2022-01-23 16:07:55 -0300

    Currency model and CurrencyAdapter created

--- modules/awesomeapi/Adapters/CurrencyAdapter.php
+++ modules/awesomeapi/Adapters/CurrencyAdapter.php
@@ -1,8 +1,49 @@
 <?php
 
+declare(strict_types=1);
+
 namespace AwesomeApi\Adapters;
 
 class CurrencyAdapter
 {
+    private ?string $code;
+    private ?string $codeIn;
+    private ?string $name;
+    private ?string $bid;
+    private ?string $createDate;
 
+    public function __construct(array $attributes)
+    {
+        $attributes = array_values($attributes);
+        $this->code = $attributes[0]['code'] ?? null;
+        $this->codeIn = $attributes[0]['codein'] ?? null;
+        $this->name = $attributes[0]['name'] ?? null;
+        $this->bid = $attributes[0]['bid'] ?? null;
+        $this->createDate = $attributes[0]['create_date'] ?? null;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function getCodeIn(): ?string
+    {
+        return $this->codeIn;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getBid(): ?int
+    {
+        return (int) str_replace('.', '', $this->bid);
+    }
+
+    public function getCreateDate(): ?string
+    {
+        return $this->createDate;
+    }
 }
--- modules/awesomeapi/Models/Currency.php
+++ modules/awesomeapi/Models/Currency.php
@@ -1,6 +1,50 @@
 <?php
 
+declare(strict_types=1);
+
+namespace AwesomeApi\Models;
+
+use AwesomeApi\Adapters\CurrencyAdapter;
+
 class Currency
 {
+    private ?string $code;
+    private ?string $codeIn;
+    private ?string $name;
+    private ?int $bid;
+    private ?string $createDate;
 
+    public function __construct(CurrencyAdapter $currencyAdapter)
+    {
+        $this->code = $currencyAdapter->getCode();
+        $this->codeIn = $currencyAdapter->getCodeIn();
+        $this->name = $currencyAdapter->getName();
+        $this->bid = $currencyAdapter->getBid();
+        $this->createDate = $currencyAdapter->getCreateDate();
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function getCodeIn(): ?string
+    {
+        return $this->codeIn;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getBid(): ?int
+    {
+        return $this->bid;
+    }
+
+    public function getCreateDate(): ?string
+    {
+        return $this->createDate;
+    }
 }
