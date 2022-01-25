de6d32e056168b50ca852e4be934728b12dd0672
vitor-melo <vitorpmelo20@gmail.com>
2022-01-24 22:50:17 -0300

    create method to list quotes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\QuoteGenerateFormRequest;
-use App\Services\PaymentService;
+use App\Services\QuoteService;
 use AwesomeApi\Services\AwesomeApiService;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\JsonResponse;
@@ -14,12 +14,12 @@ use Symfony\Component\HttpFoundation\Response;
 class HomeController extends Controller
 {
     private AwesomeApiService $awesomeApiService;
-    private PaymentService $paymentService;
+    private QuoteService $quoteService;
 
-    public function __construct(AwesomeApiService $awesomeApiService, PaymentService $paymentService)
+    public function __construct(AwesomeApiService $awesomeApiService, QuoteService $quoteService)
     {
         $this->awesomeApiService = $awesomeApiService;
-        $this->paymentService = $paymentService;
+        $this->quoteService = $quoteService;
     }
 
     public function index(): Renderable
@@ -27,14 +27,15 @@ class HomeController extends Controller
         return view('home')->with('data', $this->awesomeApiService->currenciesAvailable());
     }
 
-    public function generateQuote(QuoteGenerateFormRequest $request): JsonResponse
-    {
-        $data = $this->paymentService->quoteGenerate($request->validated());
-        return response()->json($data, Response::HTTP_OK);
-    }
-
     public function quoteHistory(): Renderable
     {
-        return view('quote-history');
+        $quotes = $this->quoteService->getQuoteHistory();
+        return view('quote-history', compact('quotes'));
+    }
+
+    public function generateQuote(QuoteGenerateFormRequest $request): JsonResponse
+    {
+        $data = $this->quoteService->quoteGenerate($request->validated());
+        return response()->json($data, Response::HTTP_OK);
     }
 }
