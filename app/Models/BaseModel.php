d10236e5228abb1e84ccd15c97607ab8ad64e8ce
vitor-melo <vitorpmelo20@gmail.com>
2022-01-24 22:55:36 -0300

    BaseModel

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -1,8 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
-class BaseModel
-{
+use Illuminate\Database\Eloquent\Model;
 
+class BaseModel extends Model
+{
+    const CREATED_AT = 'createdAt';
+    const UPDATED_AT = 'updatedAt';
 }
