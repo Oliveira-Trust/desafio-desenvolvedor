b15dc9eac9f27ecbb6b89e2115b9cd5eeb68c174
vitor-melo <vitorpmelo20@gmail.com>
2022-01-25 07:30:00 -0300

    create class Mail to send

--- app/Mail/QuoteMail.php
+++ app/Mail/QuoteMail.php
@@ -1,8 +1,27 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Mail;
 
-class QuoteMail
-{
+use App\Models\Quote;
+use Illuminate\Mail\Mailable;
 
+class QuoteMail extends Mailable
+{
+    private ?Quote $quote;
+
+    public function __construct(?Quote $quote)
+    {
+        $this->quote = $quote;
+    }
+
+    public function build()
+    {
+        $this->from('vitorpmelo20@gmail.com')
+            ->to('vitorpmelo00@gmail.com')
+            ->subject('Detalhes da Cotação');
+
+        return $this->view('mail.mail', ['data' => $this->quote]);
+    }
 }
