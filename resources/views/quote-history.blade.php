f1bdc0ea3e0877361fa1c40937c56906c09e29cd
vitor-melo <vitorpmelo20@gmail.com>
2022-01-25 08:33:33 -0300

    to refactor

--- resources/views/quote-history.blade.php
+++ resources/views/quote-history.blade.php
@@ -18,38 +18,49 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            @foreach ($quotes as $quote)
+                        @foreach ($quotes as $quote)
+                            <tr>
                                 <td>{{ $quote->currency ?? '' }}</td>
                                 <td>R${{ $quote->value ?? '' }}</td>
                                 <td>{{ $quote->finalValue ?? '' }}</td>
                                 <td>R${{ $quote->discountedValue ?? '' }}</td>
-                                <td class="text-center">
-                                    <button class="border-0" id="sendEmail">
+                                <td class="text-center justify-content-center">
+                                    <button class="border-0 bg-transparent mt-2" id="sendEmail">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
                                             <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
                                         </svg>
                                     </button>
                                 </td>
-                            @endforeach
-                        </tr>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </card>
         </div>
     </div>
-
     {!! $quotes->links() !!}
+    <div class="modal fade" tabindex="-1" id="spinnerModal">
+        <div class="modal-dialog modal-dialog-centered text-center justify-content-center">
+            <div class="spinner-border"></div>
+        </div>
+    </div>
 @endsection
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
-        $('#sendEmail').on('click', function(){
-            axios.get('{{ route('sendEmail') }}')
+        var buttonSend = $('#sendEmail');
+
+        buttonSend.on('click', function() {
+            // $('#spinnerModal').show();
+
+            {{--axios.get('{{ route('sendEmail') }}')--}}
+            axios.get('nada')
             .then((response) => {
+                $('#spinnerModal').hide();
                 console.log(response)
             })
             .catch((response) => {
+                $('#spinnerModal').hide();
                 console.log(response)
             })
         });
