19cc7156c935e15d0ef5d184d5bb56f3363195e2
vitor-melo <vitorpmelo20@gmail.com>
2022-01-24 22:55:08 -0300

    add methods in QuoteRepository

--- app/Repositories/QuoteRepository.php
+++ app/Repositories/QuoteRepository.php
@@ -1,8 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repositories;
 
-class QuoteRepository
-{
+use App\Models\Quote;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Auth;
 
+class QuoteRepository extends BaseRepository
+{
+    protected string $model = Quote::class;
+
+    public function save(array $attributes)
+    {
+        $this->createModel()->fill($attributes)->save();
+    }
+
+    public function getQuotes(int $userId): LengthAwarePaginator
+    {
+        return $this->createModel()->where('userId', $userId)->paginate(10);
+    }
 }
