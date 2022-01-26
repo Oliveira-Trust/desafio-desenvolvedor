ca7fb43aa0a97b2fe78352a27ee879c1cb43e6b1
jefferson.almeida <jefferson.almeida@osbr.com.br>
2022-01-25 21:50:49 -0300

    finalizado filtro xss com rota de verificação

--- projecto-laravel/routes/web.php
+++ projecto-laravel/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test', function() {
+    return ['status' => 'OK', request()->all()];
+})->middleware('xss-filter');
