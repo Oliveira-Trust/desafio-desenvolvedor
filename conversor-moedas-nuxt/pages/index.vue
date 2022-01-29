c6b4f7644728d0bb475d85a81decd1eea44fddc0
Haaragard <diogololz159@gmail.com>
2022-01-29 16:53:13 -0300

    alterado pagina front index

--- conversor-moedas-nuxt/pages/index.vue
+++ conversor-moedas-nuxt/pages/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <Tutorial/>
+    <Card>
+        <h1>Esta será a seção Main</h1>
+    </Card>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Card from '~/components/Card.vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { Card }
 })
 </script>
+
+<style scoped>
+
+</style>
