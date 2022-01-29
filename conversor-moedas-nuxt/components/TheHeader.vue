4fdddf7fe1f3f4840d7064ad74a9bcda083de9f6
Haaragard <diogololz159@gmail.com>
2022-01-29 16:52:31 -0300

    Adicionado component TheHeader

---
+++ conversor-moedas-nuxt/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h1>Este é o header</h1>
+        <h1>Bem-vindo ao conversor de Moedas! - made by: Haaragard</h1>
+    </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
+
+<style scoped>
+header { @apply w-full h-24 border-b px-10 py-5; }
+</style>
