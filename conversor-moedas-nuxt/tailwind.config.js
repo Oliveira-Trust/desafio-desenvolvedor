e1b06b763c7320e52c4f8b4ce1ed43a238c72abe
Haaragard <diogololz159@gmail.com>
2022-01-29 17:46:01 -0300

    adicionado config para tailwindcss e plugin para forms

---
+++ conversor-moedas-nuxt/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
+    content: [
+        './src/**/*.{html,ts,js}'
+    ]
+}
