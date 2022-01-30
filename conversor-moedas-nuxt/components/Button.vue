bb9c62e7ec06c1f8a49430a604bac7069d939ecb
Haaragard <diogololz159@gmail.com>
2022-01-30 15:01:38 -0300

    alterado component button

---
+++ conversor-moedas-nuxt/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+    <button><slot /></button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+
+})
+</script>
+
+<style scoped>
+button { @apply px-3 py-2 border rounded; }
+button:not(:disabled) { @apply hover:bg-gray-300; }
+button:disabled { @apply bg-gray-300 text-gray-600; }
+</style>
--- conversor-moedas-nuxt/nuxt.config.js
+++ conversor-moedas-nuxt/nuxt.config.js
@@ -1,52 +1,74 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+    ssr: false,
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'conversor-moedas-nuxt',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'conversor-moedas-nuxt',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [{ src: '~/plugins/vuex-persist', ssr: false }],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/typescript
+        '@nuxt/typescript-build',
+        // https://go.nuxtjs.dev/tailwindcss
+        '@nuxtjs/tailwindcss',
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        '@nuxtjs/axios',
+        '@nuxtjs/auth-next'
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/vuex-persist', ssr: false }
-  ],
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    },
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxtjs/axios'],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
-
-  axios: {
-    baseURL: 'http://localhost/api/', // Used as fallback if no runtime config is provided
-  }
+    axios: {
+        baseURL: 'http://localhost/', // Used as fallback if no runtime config is provided
+        credentials: true,
+        headers: {
+            common: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }
+    },
+    auth: {
+        strategies: {
+            local: {
+                user: {
+                    property: 'user',
+                    autoFetch: true
+                },
+                endpoints: {
+                    login: { url: 'auth/login', method: 'post' },
+                    logout: { url: 'auth/logout', method: 'post' },
+                    user: { url: 'api/user', method: 'get' }
+                }
+            }
+        }
+    }
 }
