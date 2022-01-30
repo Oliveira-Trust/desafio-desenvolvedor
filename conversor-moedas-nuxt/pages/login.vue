0fa47d4455fd3e95857803702bd7040e90bc03e5
Haaragard <diogololz159@gmail.com>
2022-01-30 15:03:46 -0300

    adicionado pagina de login

---
+++ conversor-moedas-nuxt/pages/login.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="mx-auto md:max-w-2xl">
+        <Card>
+            <h2 class="text-center text-xl">Login</h2>
+
+            <form @submit.prevent="login" method="post" action="/login">
+                <div class="mt-5 space-y-5">
+                    <div class="group-input">
+                        <label for="email">E-mail</label>
+                        <input type="email" id="email" v-model="form.email" name="email" required />
+
+                        <div v-if="errors.email" class="text-red-500 bg-red-200 mt-2 p-2 border border-red-500 rounded">
+                            <span v-for="(message, index) in errors.email" :key="index">{{ message }}</span>
+                        </div>
+                    </div>
+                    <div class="group-input">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" v-model="form.password" name="password" required />
+
+                        <div v-if="errors.password" class="text-red-500 bg-red-200 mt-2 p-2 border border-red-500 rounded">
+                            <span v-for="(message, index) in errors.password" :key="index">{{ message }}</span>
+                        </div>
+                    </div>
+                    <div class="flex items-center justify-end mt-5">
+                        <Button class="mt-5" type="submit" :disabled="!loginBusy">Login</Button>
+                    </div>
+                </div>
+            </form>
+
+        </Card>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Card from '~/components/Card.vue'
+import Button from '~/components/Button.vue'
+
+export default Vue.extend({
+    components: { Card, Button },
+    data() {
+        return {
+            form: {
+                email: '',
+                password: '',
+                remember: true
+            },
+            errors: {}
+        };
+    },
+    methods: {
+        async login() {
+            await this.$auth.loginWith('laravelSanctum', { data: this.form })
+            this.$router.push({ name: 'index' });
+        },
+        loginBusy(): Boolean {
+            return this.$auth.busy
+        }
+    }
+})
+</script>
+
+<style scoped>
+.group-input { @apply flex flex-col space-y-1; }
+input { @apply rounded; }
+</style>
