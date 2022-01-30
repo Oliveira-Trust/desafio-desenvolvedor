f00cf6718acc9d12c5da4289d30affff1644fd3f
Haaragard <diogololz159@gmail.com>
2022-01-30 11:22:04 -0300

    alterado UserSeeder para criar hash no password

--- conversor-moedas/database/seeders/UserSeeder.php
+++ conversor-moedas/database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -18,7 +19,7 @@ class UserSeeder extends Seeder
             [
                 'name' => 'Test User',
                 'email' => 'test@example.com',
-                'password' => 'password',
+                'password' => Hash::make('password'),
             ]
         ]);
 
