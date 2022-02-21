c85dff122e11dba5e46382f69d2a524c56a58e3e
Dener <dener.campos@monetizze.com.br>
2022-02-21 12:19:50 -0300

    initial commit

---
+++ oliveira-api/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/', function () {
+    // response()->json([
+    //     'message' => 'dener campos foi'
+    // ], 200);
+
+    echo "teste";
+});
