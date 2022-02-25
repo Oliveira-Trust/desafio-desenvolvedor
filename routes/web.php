f1b55e9fc8b61a516e2a0b1e58c011523071cfb7
André Filippe dos Santos <fellipe.mrt@gmail.com>
2022-02-24 22:48:08 -0300

    fix: adjusted file route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
+use App\Http\Controllers\CurrencyQuoteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+    Route::get('', [CurrencyQuoteController::class, 'index'])->name('currencyQuote.index');
+    Route::post('', [CurrencyQuoteController::class, 'toConvert'])->name('currencyQuote.toConvert');
 });
+
+require __DIR__ . '/auth.php';
