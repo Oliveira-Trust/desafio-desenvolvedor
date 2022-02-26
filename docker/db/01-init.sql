fcbfc3d0ead492e64ca1b8842984fcb953a88d17
André Filippe dos Santos <fellipe.mrt@gmail.com>
2022-02-25 22:36:14 -0300

    chore: created docker compose for local development

---
+++ docker/db/01-init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE currency_quote_testing WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
+CREATE DATABASE currency_quote WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
