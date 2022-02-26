d097125f993963180aa093da5c051b2a30819ba3
Matheus Pereira <msmatheuspereira@gmail.com>
2022-02-26 16:08:06 -0300

    adicionadno seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\MeiosPagamento;
+use App\Models\TaxasConversao;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin')
+        ]);
+
+        TaxasConversao::create([
+            'valor' => 3000,
+            'tipo' => 'maior',
+            'taxa' => 2,
+        ]);
+
+        TaxasConversao::create([
+            'valor' => 3000,
+            'tipo' => 'menor',
+            'taxa' => 1,
+        ]);
+
+        MeiosPagamento::create([
+            'meio_pagamento' => 'card',
+            'taxa' => 7.63,
+        ]);
+
+        MeiosPagamento::create([
+            'meio_pagamento' => 'billet',
+            'taxa' => 1.45,
+        ]);
     }
 }
