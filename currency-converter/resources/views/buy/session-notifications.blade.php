0f20330ab3d2481350ac3704432adef13ae85b51
Eduardo (eduao) Oliveira <emaildoedu00@gmail.com>
2022-03-22 05:55:15 -0300

    build: :wrench: fix docker

--- currency-converter/resources/views/buy/session-notifications.blade.php
+++ currency-converter/resources/views/buy/session-notifications.blade.php
@@ -7,7 +7,6 @@
         <p class="msg">Payment Value: {{session('buyStatus')->value}}</p>
         <p class="msg">Convertion Fee: {{session('buyStatus')->convertion_fee}}</p>
         <p class="msg">Payment Fee: {{session('buyStatus')->payment_fee}}</p>
-        <td></td>
         <p class="msg">Total Received: {{session('buyStatus')->selling_price}}</p>
         @endif
 
