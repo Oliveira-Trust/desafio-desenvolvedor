9947c713f27e00997db7f8ba904d7a38bb829c0b
David dos Santos Machado <david.desenvolvedor@gmail.com>
2022-07-05 05:23:44 -0300

    :recycle: refactor: enviando o usuário para o histórico ao realizar login

--- currency_app/app/Providers/RouteServiceProvider.php
+++ currency_app/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/historico';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
