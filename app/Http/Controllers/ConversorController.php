5bc4164ad80e955a444bc75e0febcc0ff70dd4e1
Felipe Ramon <rammonfelip@gmail.com>
2023-01-04 21:49:55 -0300

    Adicionando CotacaoService.

--- app/Http/Controllers/ConversorController.php
+++ app/Http/Controllers/ConversorController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ConversorRequest;
-use Illuminate\Http\Request;
+use App\Services\CotacaoService;
 
 class ConversorController extends Controller
 {
     public function conversor(ConversorRequest $request)
     {
-        return $request->all();
+        $cotacao = new CotacaoService($request->input('valor'), $request->input('moeda'), $request->input('pagamento'));
+        $data = $cotacao->converterMoeda();
+        return response()->json([
+            'data'  =>  [
+                'success'   =>  true,
+                'info'  =>  $data
+            ]
+        ]);
     }
 }
---
+++ app/Services/CotacaoService.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class CotacaoService
+{
+    private $cotacao;
+    private $taxaConversao;
+    private $taxaPagamento;
+
+    public function __construct($valor, $moeda, $pagamentoTipo)
+    {
+        $this->valor = floatval($valor);
+        $this->moeda = $moeda;
+        $this->pagamentoTipo = $pagamentoTipo;
+        $this->cotacao = $this->getCotacaoMoeda($moeda);
+        $this->taxaConversao = $this->setTaxaConversao($valor);
+        $this->taxaPagamento = $this->setTaxaPagamento($pagamentoTipo);
+    }
+
+    public function converterMoeda()
+    {
+        $txPagamento = $this->getTaxaPagamento();
+        $txConversao = $this->getTaxaConversao();
+
+        $taxas = [
+            'pagamento'    =>  $txPagamento,
+            'conversao'    =>  $txConversao,
+            'total'       =>  ($txPagamento + $txConversao),
+        ];
+
+        return [
+            'origem'    =>  'BRL',
+            'destino'   =>  $this->cotacao['code'],
+            'valor_moeda'   =>  round($this->cotacao['bid'], 2),
+            'valor_conversao'   => $this->valor,
+            'valor_convertido' =>  $this->valor - $taxas['total'],
+            'moeda_comprada'    =>  round(($this->valor - $taxas['total']) / round($this->cotacao['bid'], 2), 2), //Considerando o valor descontado as taxas e dividindo pelo valor da moeda destino
+            'taxas' =>  $taxas
+        ];
+    }
+
+    private function getCotacaoMoeda($moeda)
+    {
+        $url = env('URL_API_COTACAO') . $moeda;
+        $response = Http::acceptJson()->get($url);
+
+        $this->cotacao = $response->json($moeda.'BRL');
+
+        return $this->cotacao;
+    }
+
+    private function getTaxaPagamento()
+    {
+        return $this->valor * $this->taxaPagamento;
+    }
+
+    private function setTaxaPagamento($pagamentoTipo)
+    {
+        $taxas = [
+            'boleto'    =>  0.0145,
+            'cartao'    =>  0.0763
+        ];
+
+        $this->taxaPagamento = $taxas[$pagamentoTipo];
+
+        return $this->taxaPagamento;
+    }
+
+    private function getTaxaConversao()
+    {
+        return $this->valor * $this->taxaConversao;
+    }
+
+    private function setTaxaConversao($valor)
+    {
+        $this->taxaConversao = ($valor >= 3000) ? 0.01 : 0.02;
+
+        return $this->taxaConversao;
+    }
+}
