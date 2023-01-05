0e057d3f4619233e2191aa7f0308e50862016da5
Felipe Ramon <rammonfelip@gmail.com>
2023-01-05 13:16:50 -0300

    Criando migrations e Seeder para gerenciar taxas

---
+++ database/migrations/2023_01_05_020546_create_table_pagamento.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pagamento', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->nullable();
+            $table->string('nome');
+            $table->string('slug');
+            $table->decimal('taxa', '8', 4);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pagamento');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Pagamento;
+use App\Models\Taxa;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $users = [
+            ['id' => 1, 'name' =>  'Admin', 'email' => 'admin@oliveiratrust.com', 'password' => 'admin@oliveiratrust.com'],
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $pagamentos = [
+            ['id' => 1, 'nome' =>  'Boleto',            'slug' => 'boleto',             'taxa' => 1.45],
+            ['id' => 2, 'nome' =>  'Cartão de Crédito', 'slug' => 'cartao-de-credito',  'taxa' => 7.63],
+        ];
+
+        $taxas = [
+            ['id' => 1, 'valor' =>  1000, 'taxa' => 2],
+            ['id' => 2, 'valor' =>  3000, 'taxa' => 1],
+        ];
+
+        foreach ($users as $user) {
+            $user = User::updateOrCreate([
+                'id'    =>  $user['id']
+            ], $user);
+
+            foreach ($pagamentos as $pagamento) {
+                $pagamento['user_id'] = $user->id;
+
+                Pagamento::updateOrCreate([
+                    'id'    =>  $pagamento['id']
+                ], $pagamento);
+            }
+
+            foreach ($taxas as $taxa) {
+                $taxa['user_id'] = $user->id;
+
+                Taxa::updateOrCreate([
+                    'id'    =>  $taxa['id']
+                ], $taxa);
+            }
+        }
     }
 }
