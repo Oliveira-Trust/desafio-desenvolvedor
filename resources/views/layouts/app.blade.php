a91b7a3e6f405d547dfd305bb4e77f776cf83168
paulo <paulofullstack@hotmail.com>
2023-02-09 12:42:33 -0300

    Adicionando importacoes javascript

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,7 +98,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
-    <script src="/js/scripts.js"></script>
+    <script type="module" src="/js/scripts.js"></script>
+    <script src="/js/addMask.js"></script>
+    <script src="/js/clearInput.js"></script>
 </body>
 
 </html>
