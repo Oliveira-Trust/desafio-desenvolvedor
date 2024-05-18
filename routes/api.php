67da73a34ed129f2083f77d691119b2c5f4f591f
Cassius Leon Lopes Cutrim <cassiuslc@gmail.com>
2024-05-18 16:33:20 -0400

    feat: Finalize routes for JWT authentication

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ExemploMail;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
+/**
+ * Authentication routes for the API
+ *
+ * This group of routes is used for authentication and authorization of the API.
+ * It allows the user to login, logout, refresh and retrieve their current information.
+ */
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login-api');
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout-api');
+    Route::post('refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh-api');
+    Route::get('me', [AuthController::class, 'me'])->middleware('auth:api')->name('me-api');
+});
 
 Route::get('/testar-email', function () {
     $mailData = [
