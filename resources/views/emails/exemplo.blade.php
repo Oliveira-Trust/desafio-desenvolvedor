7e744583c5f7c3f9e46f55975e79dc16f5581f12
Cassius Leon Lopes Cutrim <cassiuslc@gmail.com>
2024-05-18 15:56:19 -0400

    fix: Fix Mailtrap configuration for JWT authentication feature

---
+++ resources/views/emails/exemplo.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 11 Send Email with Attachment Example - Techsolutiontuff</title>
+</head>
+<body>
+    <h1>{{ $mailData['title'] }}</h1>
+  
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.</p>
+     
+    <p>Thank you</p>
+</body>
+</html>
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ExemploMail;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/testar-email', function () {
+    $mailData = [
+        'title' => 'This is Test Mail'
+    ];
+
+    Mail::to('email@example.com')->send(new ExemploMail($mailData));
+    return 'E-mail enviado com sucesso!';
+});
