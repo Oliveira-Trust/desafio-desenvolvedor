4965538eb8f8b18799d0dc60bf48477649d40acc
Kevin Alcantara <42805770+kvn-alcantara@users.noreply.github.com>
2024-05-19 20:02:41 -0300

    Update requested.blade.php

--- resources/views/emails/conversions/requested.blade.php
+++ resources/views/emails/conversions/requested.blade.php
@@ -6,9 +6,9 @@
     Valor da "Moeda de destino" usado para conversão: {{ $conversion['bid'] }} <br>
     Taxa de pagamento: {{ $conversion['payment_fee'] }} <br>
     Taxa de conversão: {{ $conversion['amount_fee'] }} <br>
-    Valor utilizado para conversão descontando as taxas: {{ $conversion['target_amount'] - $conversion['amount_fee'] - $conversion['payment_fee'] }} <br>
+    Valor utilizado para conversão descontando as taxas: {{ $conversion['amount'] - $conversion['amount_fee'] - $conversion['payment_fee'] }} <br>
     <hr>
     <b>Valor comprado em "Moeda de destino":</b> {{ $conversion['target_amount'] }}
     <hr>
-    <small>Cotação realizada em {{ $conversion['created_at']->format('d/m/Y H:i:s') }}</small>
+    <small>Cotação realizada em {{ $conversion['created_at'] }}</small>
 </div>
