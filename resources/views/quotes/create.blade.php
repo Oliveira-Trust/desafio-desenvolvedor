d671a4ecf4eadecb33c48f031a462176e8390399
edRibeiro <edersonribeiro.ita@gmail.com>
2024-05-21 01:45:28 -0300

    doc: Criando documento de informações sobre o projeto.

--- resources/views/quotes/create.blade.php
+++ resources/views/quotes/create.blade.php
@@ -175,10 +175,10 @@
                         <!-- Button -->
                         <div class="mt-5 flex justify-end gap-x-2">
                             <div class="mt-5 flex justify-end gap-x-2">
-                                <button type="button"
+
+                                <a href="{{ route('quotes.index') }}"
                                     class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
-                                    Cancel
-                                </button>
+                                    Cancelar</a>
                                 <button type="submit" id="buyButton" disabled form="quote_form"
                                     class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
                                     Comprar
