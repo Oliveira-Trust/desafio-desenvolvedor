4813e74918194d63b2a4821b898a1904d0d6726d
Cassius Leon Lopes Cutrim <cassiuslc@gmail.com>
2024-05-30 21:16:41 -0400

    chore: Update Redis configuration for session, queue, cache, and currency service

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,5 +4,6 @@ return [
     App\Providers\AppRepositoriesProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
 ];
