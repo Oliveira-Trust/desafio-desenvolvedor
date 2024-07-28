643666f7df1457d17a1a85881248d08a2ab2df02
Felipe Rosa <feliperosareis@gmail.com>
2024-07-28 09:29:59 -0300

    Diagramada a tela de conversão

---
+++ conversor/app/Http/Controllers/ConversorController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConversorController extends Controller
+{
+    function index() {
+        return view('home');
+    }
+}
--- conversor/routes/web.php
+++ conversor/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\ConversorController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ConversorController::class, 'index']);
