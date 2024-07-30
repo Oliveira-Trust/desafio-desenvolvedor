c17d18c9d528c463b580754aa5295d33e4bf5a72
David dos Santos Machado <david.s.machado99@gmail.com>
2024-07-30 05:32:59 -0300

    feat(conversion): adding routes to project

--- currency-api/routes/api.php
+++ currency-api/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::middleware(['auth:sanctum'])->get('/available-coins', [\App\Http\Controllers\CurrencyConversionController::class, 'getAvailableCoins']);
+    Route::middleware(['auth:sanctum'])->get('/history', [\App\Http\Controllers\CurrencyConversionController::class, 'getHistory']);
+    Route::middleware(['auth:sanctum'])->post('/convert', [\App\Http\Controllers\CurrencyConversionController::class, 'convert']);
 });
