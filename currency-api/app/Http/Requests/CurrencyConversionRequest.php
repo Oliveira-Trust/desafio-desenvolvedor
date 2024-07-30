c79d562dae52205b7561c6d8eaca2b9e348b1179
David dos Santos Machado <david.s.machado99@gmail.com>
2024-07-30 03:56:24 -0300

    feat(conversion): creating form validation for conversion

---
+++ currency-api/app/Http/Requests/CurrencyConversionRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CurrencyConversionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'target_coin' => 'required|string',
+            'payment_method' => 'required|string|exists:payment_methods,id',
+            'value' => 'required|numeric|between:1000,100000'
+        ];
+    }
+}
