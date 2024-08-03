9bac97710abd276fe4189f113538a6be68eada37
David dos Santos Machado <david.s.machado99@gmail.com>
2024-08-03 02:25:17 -0300

    feat(auth): addingg login page

--- currency-app/src/presentation/pages/LoginPage.vue
+++ currency-app/src/presentation/pages/LoginPage.vue
@@ -4,14 +4,18 @@ import router from '@/router';
 import { mdiEmailOutline, mdiEye, mdiEyeOff, mdiLockOutline } from '@mdi/js';
 import { ref } from 'vue';
 import InputField from '../components/shared/form/InputField.vue';
+import UnprocessableEntityException from "@/infrastructure/http/exceptions/unprocessable-entity-exception";
+import logoOt from '@/assets/logo.svg';
+import {useToastStore} from "@/core/stores/toast.store";
 
 const authStore = useAuthStore();
 
 const form = ref(false);
 const visible = ref(false);
-const username = ref('');
+const email = ref('');
 const password = ref('');
 const loading = ref(false);
+const errors = ref({} as {email: string[], password: string[]});
 
 const requiredRule = (value: string) => !!value || 'Campo obrigatório';
 const usernameRules = ref([requiredRule]);
@@ -22,11 +26,14 @@ async function onLoginHandler() {
         loading.value = true;
 
         try {
-            await authStore.login(username.value, password.value);
-            router.push({ name: 'home' });
+            await authStore.login(email.value, password.value);
+            await router.push({name: 'home'})
         } catch (error: unknown) {
-            if (error instanceof Error) {
-                console.log(error.message);
+            if (error instanceof UnprocessableEntityException) {
+                // TODO: Aplicar highlight nos inputs de erro
+                errors.value = error.errors;
+            } else if (error instanceof Error) {
+                useToastStore().error(error.message);
             }
         }
 
@@ -37,16 +44,22 @@ async function onLoginHandler() {
 
 <template>
     <div class="page">
-        <VCard class="card" elevation="8" rounded="lg">
+        <VCard class="card" elevation="2" rounded="lg">
+          <VImg
+              class="logo"
+              max-width="240"
+              :src="logoOt"
+          />
             <VForm v-model="form" @submit.prevent="onLoginHandler">
                 <VRow>
                     <VCol cols="12">
                         <InputField
-                            v-model="username"
+                            v-model="email"
                             label="Email"
                             :prepend-inner-icon="mdiEmailOutline"
+                            :error-messages="errors.email"
                             :rules="usernameRules"
-                            hide-details
+                            hide-details="auto"
                         />
                     </VCol>
                     <VCol cols="12">
@@ -56,14 +69,15 @@ async function onLoginHandler() {
                             :type="visible ? 'text' : 'password'"
                             :prepend-inner-icon="mdiLockOutline"
                             :append-inner-icon="visible ? mdiEyeOff : mdiEye"
+                            :error-messages="errors.password"
                             :rules="passwordRules"
-                            hide-details
+                            hide-details="auto"
                             @click:append-inner="visible = !visible"
                         />
                     </VCol>
                 </VRow>
 
-                <VBtn class="mt-10" type="submit" color="blue" size="large" variant="tonal" block text="Entrar" />
+                <VBtn class="mt-10" type="submit" color="blue" size="large" variant="tonal" block text="Entrar" :loading />
             </VForm>
         </VCard>
     </div>
@@ -80,6 +94,11 @@ async function onLoginHandler() {
     .card {
         width: 448px;
         padding: 48px;
+
+      .logo {
+        margin-inline: auto;
+        margin-bottom: 36px;
+      }
     }
 }
 </style>
