d9a1a86624b28b5e0bd529d12b2e58f95a222078
David dos Santos Machado <david.s.machado99@gmail.com>
2024-08-03 02:27:00 -0300

    feat(views): creating home page

--- currency-app/src/presentation/pages/HomePage.vue
+++ currency-app/src/presentation/pages/HomePage.vue
@@ -1,5 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {mdiPlus} from "@mdi/js";
+import CreateExchange from "../components/CreateExchange.vue";
+import ExchangesTable from "../components/ExchangesTable.vue";
+</script>
 
 <template>
-    <div>Home Page</div>
+    <div>
+        <div class="header">
+            <div class="header__title">Conversões</div>
+            <CreateExchange>
+                <template #activator="{ props }">
+                    <VBtn v-bind="props" :prepend-icon="mdiPlus" text="Nova conversão" variant="flat" color="primary" rounded="lg"/>
+                </template>
+            </CreateExchange>
+        </div>
+        <ExchangesTable />
+    </div>
 </template>
+
+<style scoped lang="scss">
+.header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 24px;
+    &__title {
+        font-size: 24px;
+        font-weight: 500;
+        text-transform: uppercase;
+    }
+}
+</style>
