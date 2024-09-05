15e5a07050072e97908133befc7bf1b4c6c3f09c
FIXER3600 <guilhermecomercial51@gmail.com>
2024-09-05 11:26:51 -0300

    feat: adicionando rota de importação de arquivos

---
+++ upload-csv/app/Http/Controllers/UserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    public function import(){
+        return;
+    }
+}
--- upload-csv/routes/web.php
+++ upload-csv/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/users-import', [UserController::class,'import'])->name('user.import');
