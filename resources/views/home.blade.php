4daeb6f91de629a9c57cadfe831a03ad0f284520
João Vitor Santana <jvs4nt@Joaos-Macbook-Air.local>
2024-09-06 01:49:41 -0300

    redirecionamento home + ajustes home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card mb-5">
                 <div class="card-header">{{ __('Introdução') }}</div>
 
                 <div class="card-body">
@@ -15,9 +15,24 @@
                     @endif
                     <div class="mb-4">
                         <h2>Seja bem-vindo(a), {{ \Illuminate\Support\Facades\Auth::user()->name }}!</h2>
+                        <p>Este desafio foi desenvolvido para a vaga Desenvolvedor PHP Júnior da empresa <a href="https://www.oliveiratrust.com.br" target="_blank" class="text-bg-danger px-1 rounded-2">Oliveira Trust</a></p>
                     </div>
                     <div>
                         <a class="btn btn-dark" href="{{ route('cambio.index') }}">Iniciar Sistema</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">{{ __('Sobre mim...') }}</div>
+
+                <div class="card-body">
+                    <div class="mb-4">
+                        <h5>Prazer, sou o João Vitor Santos!</h5>
+                        <p>Tenho 20 anos, atuo como Desenvolvedor Full-Stack para uma consultoria de softwares. Comecei meus estudos na área de TI no começo de 2020, quando passei no processo seletivo para realizar o ensino médio integrado ao ensino técnico de Informática para Internet pela ETEC. Hoje estou cursando Análise e Desenvolvimento de Sistemas pela FIAP (Av. Paulista). Pretendo evoluir muito na área e me especializar ainda mais nos próximos anos.</p>
+                    </div>
+                    <div>
+                        <a class="btn btn-info border border-black" href="https://www.linkedin.com/in/jo%C3%A3o-santos-3b02a5220/" target="_blank">LinkedIn</a>
+                        <a class="btn btn-dark border border-black" href="https://github.com/jvs4nt" target="_blank">Github</a>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ route('home') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <a class="btn btn-light " href="{{ route('logs.index') }}">Registro de logs</a>
