c9232c8f98e8b4a35c540baaad06fb5029d5ca87
Rodrigo Medeiros Grassioto <rodrigomgrassioto@gmail.com>
2024-09-06 17:01:16 -0300

    refactor: Remove functions not used.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,100 +7,16 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\Request;
+use function Pest\Laravel\json;
 
 class AuthController extends Controller
 {
-    public function verifyEmail(Request $request) //from web route
-    {
-        auth()->loginUsingId($request->route('id')); //make login
-
-        //test hash doesn't belong to the user
-        if ($request->route('id') != $request->user()->getKey()) {
-//            return response('Hash não pertence ao usuário', 401 );
-            if (env('APP_DEBUG') === false){
-                return redirect(env('APP_URL').'/login?verified=incorrect-user');
-            }
-            else{
-                return redirect(env('APP_URL').':9000/login?verified=incorrect-user');
-            }
-        }
-
-        //check if the email is already verified
-        if ($request->user()->hasVerifiedEmail()) {
-//            return response(['message'=>'Already verified']);
-            if (env('APP_DEBUG') === false){
-                return redirect(env('APP_URL').'/login?verified=old');
-            }
-            else{
-                return redirect(env('APP_URL').':9000/login?verified=old');
-            }
-        }
-
-        //mark as verified email
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
-            if (env('APP_DEBUG') === false){
-                return redirect(env('APP_URL').'/login?verified=ok');
-            }
-            else{
-                return redirect(env('APP_URL').':9000/login?verified=ok');
-            }
-        }
-        //nothing answers
-        if (env('APP_DEBUG') === false){
-            return redirect(env('APP_URL').'/login?verified=noting');
-        }
-        else{
-            return redirect(env('APP_URL').':9000/login?verified=noting');
-        }
-    }
-
-    public function resendVerifyEmail(Request $request)
-    {
-//        return response($request, 499);
-//        return response(['message' => 'chegou aqui'], 499);
-//        $request = $request->only('email');
-//        return response($request, 499);
-        $user = User::firstWhere('email', $request->email);
-        $user->sendEmailVerificationNotification();
-        return response(['message' => "Email de confirmação enviado para ".$request['email'] ]);
-    }
-
     public function login(Request $request)
     {
-//        return response()->json($request,499);
         $request = $request->only(['email', 'password']);
-        if (!auth()->attempt($request)) abort(401, 'Dados não conferem');
+        if (!auth()->attempt($request)) return response()->json(['message' => 'Dados não conferem.'],401);
         $user = User::where('email',$request['email'])
             ->first();
-
-        if ($user->active != true) abort(401,'Usuário desativado');
-
-        $resources = $user->role->resources;
-        $resources = $resources->toArray();
-
-        // Ordena o array pelo campo 'name'
-        usort($resources, function($a, $b) {
-            return strcmp($a['name'], $b['name']);
-        });
-
-        // Encontra o índice do objeto com 'name' igual a 'Dashboard'
-        $dashboardIndex = array_search('Dashboard', array_column($resources, 'name'));
-
-        // Move o objeto 'Dashboard' para a primeira posição
-        if ($dashboardIndex !== false) {
-            $dashboard = $resources[$dashboardIndex];
-            unset($resources[$dashboardIndex]);
-            array_unshift($resources, $dashboard);
-        }
-
-        if (!$user->email_verified_at) return response()->json([
-            'message' =>  "Favor confirmar o e-mail $user->email!",
-            'data' => [
-                'user' => $user,
-                'resources' => $resources,
-            ],
-        ]);
 
         if ($expirationMinutes = config('sanctum.expiration')) {
             $expirationMinutes = (int) $expirationMinutes;
@@ -113,7 +29,6 @@ class AuthController extends Controller
                 'message' =>  "Bem vindo(a) $user->name!",
                 'data' => [
                     'user' => $user,
-                    'resources' => $resources,
                     'token' => auth()->user()->createToken($request['email'],['*'],$expiresAt)->plainTextToken,
                 ],
 
@@ -122,9 +37,6 @@ class AuthController extends Controller
     }
     public function logout(Request $request)
     {
-//        return response()->json(['message' => "chegou"], 499);
-
-//        $this->guard->logout();
         auth()->user()->tokens()->delete();
 
         return response()->json([
