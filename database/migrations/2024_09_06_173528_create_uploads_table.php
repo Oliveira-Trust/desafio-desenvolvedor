a05fae732545f824d53535f4a4b774064b48f521
Rodrigo Medeiros Grassioto <rodrigomgrassioto@gmail.com>
2024-09-06 15:02:52 -0300

    chore: remove timestamps columns

--- database/migrations/2024_09_06_173528_create_uploads_table.php
+++ database/migrations/2024_09_06_173528_create_uploads_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('file_name');
             $table->timestamp('uploaded_at');
-            $table->timestamps();
+            // $table->timestamps();
         });
     }
 
