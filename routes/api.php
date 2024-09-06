8da33c87d5cf6ef46a5c03808ca414ef4afd9fe0
Rodrigo Medeiros Grassioto <rodrigomgrassioto@gmail.com>
2024-09-06 17:35:51 -0300

    Feat: Update API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+route::post('login', [AuthController::class, 'login']);
+
+// AUTENTICADO
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('upload', [FileController::class, 'upload']);
+    Route::get('upload-history', [FileController::class, 'uploadHistory']);
+    Route::get('search-content', [FileController::class, 'searchContent']);
+});
+// AUTENTICADO FIM
