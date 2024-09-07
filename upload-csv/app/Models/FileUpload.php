36b4cdbbd5a14320311832588f6a3227c7694d6a
FIXER3600 <guilhermecomercial51@gmail.com>
2024-09-07 20:59:11 -0300

    Feat: model de FileUpload

--- upload-csv/app/Models/FileUpload.php
+++ upload-csv/app/Models/FileUpload.php
@@ -2,10 +2,62 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Client as Mongo;
 
-class FileUpload extends Model
+class FileUpload
 {
-    use HasFactory;
+    protected $mongoClient;
+    protected $db;
+
+    public function __construct()
+    {
+        try {
+            $this->mongoClient = new Mongo(env('MONGO_URI'));
+            $this->db = $this->mongoClient->selectDatabase('fileupload');
+        } catch (\Exception $e) {
+            throw new \Exception('Erro ao conectar ao MongoDB: ' . $e->getMessage());
+        }
+    }
+
+    public function findFileByHash($fileHash)
+    {
+        return $this->db->file_upload->findOne(['file_hash' => $fileHash]);
+    }
+
+    public function saveFileUpload($fileData)
+    {
+        return $this->db->file_upload->insertOne($fileData);
+    }
+
+    public function findFileById($fileId)
+    {
+        return $this->db->file_upload->findOne(['_id' => new \MongoDB\BSON\ObjectId($fileId)]);
+    }
+
+    public function findAllFiles()
+    {
+        return $this->db->file_upload->find()->toArray();
+    }
+
+    public function findFilesByCriteria($filter)
+    {
+        return $this->db->file_upload->find($filter)->toArray();
+    }
+
+    public function insertFileData($preparedBatchData)
+    {
+        return $this->db->file_data->insertMany($preparedBatchData);
+    }
+
+    public function searchFiles($filter, $skip, $limit)
+    {
+        $cursor = $this->db->file_data->find($filter, [
+            'skip' => $skip,
+            'limit' => $limit
+        ]);
+        $documents = $cursor->toArray();
+        $total = $this->db->file_data->countDocuments($filter);
+
+        return ['documents' => $documents, 'total' => $total];
+    }
 }
