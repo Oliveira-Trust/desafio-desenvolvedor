0b9c203e11710f85dc2686d3edd6c03addb16f9d
Eduarda <eduarda@houpa.app>
2024-09-07 13:45:26 -0300

    [eduardaCirinaRosaSantos] - Criando controller e helper para o metodo upload

---
+++ apiUploads/app/Helpers/FileHelper.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\Request;
+
+class FileHelper
+{
+    public static function validateUpload(Request $request)
+    {
+       
+    }
+}
---
+++ apiUploads/app/Http/UploadController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Upload;
+use Illuminate\Support\Facades\Storage;
+use App\Helpers\FileHelper;
+
+class UploadController extends Controller
+{
+    public function upload(Request $request)
+    {
+        
+    }
+}
