beb7ab19de4b3063172f9300dfdb2acc33ffaaf2
Rodrigo Medeiros Grassioto <rodrigomgrassioto@gmail.com>
2024-09-07 00:19:56 -0300

    Test: Test Undefined array key \"TckrSymb\""

--- app/Imports/FileImport.php
+++ app/Imports/FileImport.php
@@ -4,8 +4,10 @@ namespace App\Imports;
 
 use App\Models\FileContent;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
 
-class FileImport implements ToModel
+class FileImport implements ToModel, WithChunkReading, WithBatchInserts
 {
     protected $uploadId;
 
@@ -14,13 +16,9 @@ class FileImport implements ToModel
         $this->uploadId = $uploadId;
     }
 
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
     public function model(array $row)
     {
+        dd($row); // Undefined array key \"TckrSymb\"" ??
         return new FileContent([
             'tckr_symb' => $row['TckrSymb'],
             'rpt_dt' => $row['RptDt'],
@@ -30,5 +28,25 @@ class FileImport implements ToModel
             'crpn_nm' => $row['CrpnNm'],
             'upload_id' => $this->uploadId,
         ]);
+    }
+
+    /**
+     * Número de linhas por lote
+     *
+     * @return int
+     */
+    public function chunkSize(): int
+    {
+        return 1000;
+    }
+
+    /**
+     * Número de registros por batch de inserção
+     *
+     * @return int
+     */
+    public function batchSize(): int
+    {
+        return 1000;
     }
 }
