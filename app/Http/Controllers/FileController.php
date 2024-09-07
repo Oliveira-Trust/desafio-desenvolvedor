4968ce64cc275fa826751c7012004624857bc08d
Rodrigo Medeiros Grassioto <rodrigomgrassioto@gmail.com>
2024-09-07 19:43:48 -0300

    Fix: Save file whith name, send file name to Job(fila). Disabled verification end update encoding

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -24,7 +24,7 @@ class FileController extends Controller
     public function upload(FileRequest $request)
     {
         $file = $request->file('file');
-        $this->_detectFileEncoding($file);
+//        $this->_detectFileEncoding($file);
         $fileName = $file->getClientOriginalName();
 
         // Verificar se o arquivo já foi enviado
@@ -33,10 +33,12 @@ class FileController extends Controller
 
         // Converte o arquivo para UTF-8
         $path = $file->getRealPath();
-        $utf8FilePath = $this->convertToUtf8($file);
+//        $this->convertToUtf8($path);
 
         // Salvar o arquivo
-        $filePath = $file->store('files');
+//        $filePath = $file->store('files');
+        $filePath = $file->storeAs('files', $fileName);
+
 
         // Criar registro de upload
         $upload = Upload::create([
@@ -45,10 +47,11 @@ class FileController extends Controller
         ]);
 
         // Importar e salvar o conteúdo do arquivo
-        $this->excel->import(new FileImport($upload->id), $utf8FilePath); // uso total memória.
+//        dd(mb_detect_encoding($filePath, mb_list_encodings(), true));
+//        $this->excel->import(new FileImport($upload->id), $filePath); // uso total memória.
 
         // Disparar o job para processar o arquivo
-//        ProcessFileImport::dispatch($file, $upload->id);
+        ProcessFileImport::dispatch($upload->id, $filePath, $fileName );
 
         return response()
             ->json(['message' => 'Arquivo carregado. Será processado em fila. Acompanhe o resultado no logs.']);
@@ -88,23 +91,15 @@ class FileController extends Controller
 
     private function convertToUtf8($filePath)
     {
-        $utf8FilePath = $filePath . '.utf8.csv';
+        $content = file_get_contents($filePath);
 
-        // Abre o arquivo original
-        $originalFile = fopen($filePath, 'r');
-        // Abre o novo arquivo para escrita em UTF-8
-        $utf8File = fopen($utf8FilePath, 'w');
-
-        while (($line = fgets($originalFile)) !== false) {
-            // Converte a linha para UTF-8
-            $utf8Line = mb_convert_encoding($line, 'UTF-8', 'auto');
-            fwrite($utf8File, $utf8Line);
+        // Detect encoding and convert to UTF-8 if necessary
+        $encoding = mb_detect_encoding($content, mb_list_encodings(), true);
+        if ($encoding && $encoding !== 'UTF-8') {
+            $content = mb_convert_encoding($content, 'UTF-8', $encoding);
+            // Save the file with UTF-8 encoding
+            file_put_contents($filePath, $content);
         }
-
-        fclose($originalFile);
-        fclose($utf8File);
-
-        return $utf8FilePath;
     }
 
     function _detectFileEncoding($filepath) {
