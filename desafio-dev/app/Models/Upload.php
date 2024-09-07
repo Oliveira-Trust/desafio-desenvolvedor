21d0a8654f473e0462680ed25f96e7ae2e0256c6
alefsalu <alefbssalu@hotmail.com>
2024-09-07 00:42:55 -0300

    criação model Upload

---
+++ desafio-dev/app/Models/Upload.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Upload extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['file_name', 'file_path', 'file_hash'];
+}
