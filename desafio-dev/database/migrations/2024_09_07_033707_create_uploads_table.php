611075e2f425c3f37471554542935facb800af13
alefsalu <alefbssalu@hotmail.com>
2024-09-07 00:39:31 -0300

    criação migration tabela uploads

---
+++ desafio-dev/database/migrations/2024_09_07_033707_create_uploads_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('uploads', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->string('file_hash')->unique();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('uploads');
+    }
+};
