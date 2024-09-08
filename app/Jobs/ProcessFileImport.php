431d89a38958dfb5a366d6fc2e5163428bad6b41
Rodrigo Medeiros Grassioto <rodrigomgrassioto@gmail.com>
2024-09-08 15:57:11 -0300

    Feat: Gerando relatório sobre progresso da importação em html

--- app/Jobs/ProcessFileImport.php
+++ app/Jobs/ProcessFileImport.php
@@ -10,6 +10,8 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Carbon;
 
 class ProcessFileImport implements ShouldQueue
 {
@@ -18,33 +20,78 @@ class ProcessFileImport implements ShouldQueue
     protected $uploadId;
     protected $filePath;
     protected $fileName;
+    protected $verifyMinutes;
 
-
-    public function __construct($uploadId, $filePath, $fileName)
+    public function __construct($uploadId, $filePath, $fileName, $verifyMinutes = 3)
     {
         $this->uploadId = $uploadId;
         $this->filePath = $filePath;
         $this->fileName = $fileName;
+        $this->verifyMinutes = $verifyMinutes;
     }
 
-    /**
-     * Execute the job.
-     *
-     * @return void
-     */
     public function handle()
     {
-        try {
-            Log::info('Iniciando importação para o arquivo: ' . $this->fileName);
+        $this->createReportFile('Iniciando a importação de ' . $this->fileName . ' com ' . $this->countTotalRows() . ' linhas');
 
-            // Importar o arquivo
-            Excel::import(new FileImport($this->uploadId), $this->filePath);
-//            $this->excel->import(new FileImport($upload->id), $filePath);
+        $rowsProcessed = 0;
+        $startTime = Carbon::now();
+        $endTime = $startTime->copy()->addMinutes($this->verifyMinutes);
 
-            Log::info('Importação concluída para o arquivo: ' . $this->filePath->getClientOriginalName());
-        } catch (\Exception $e) {
-            Log::error('Erro ao importar o arquivo: ' . $e->getMessage());
-            throw $e;
+        while (true) {
+            try {
+                Excel::import(new FileImport($this->uploadId), $this->filePath);
+
+
+                $rowsProcessed = $this->countProcessedRows();
+                $this->updateReportFile('Processado ' . $rowsProcessed . ' de ' . $this->countTotalRows() . ' linhas');
+
+                
+                if (Carbon::now()->greaterThanOrEqualTo($endTime)) {
+                    $endTime = Carbon::now()->addMinutes($this->verifyMinutes);
+                    $rowsProcessed = $this->countProcessedRows();
+                    $this->updateReportFile('Processado ' . $rowsProcessed . ' de ' . $this->countTotalRows() . ' linhas');
+                }
+            } catch (\Exception $e) {
+                Log::error('Erro ao importar o arquivo: ' . $e->getMessage());
+                $this->createReportFile('Erro ao importar o arquivo: ' . $e->getMessage());
+                throw $e;
+            }
+        }
+
+        $this->updateReportFile('Arquivo ' . $this->fileName . ' com ' . $this->countTotalRows() . ' registros processados com sucesso');
+    }
+
+    protected function countTotalRows()
+    {
+        $spreadsheet = Excel::toCollection(null, $this->filePath);
+        return $spreadsheet->first()->count();
+    }
+
+    protected function countProcessedRows()
+    {
+        return \App\Models\FileContent::where('upload_id', $this->uploadId)->count();
+    }
+
+    protected function createReportFile($description)
+    {
+        $reportPath = 'public/report_jobs/' . pathinfo($this->fileName, PATHINFO_FILENAME) . '.html';
+        $content = "<html><body><table border='1'>";
+        $content .= "<tr><th>Data e Hora</th><th>Descrição</th></tr>";
+        $content .= "<tr><td>" . now() . "</td><td>" . $description . "</td></tr>";
+        $content .= "</table></body></html>";
+
+        Storage::put($reportPath, $content);
+    }
+
+    protected function updateReportFile($description)
+    {
+        $reportPath = 'public/report_jobs/' . pathinfo($this->fileName, PATHINFO_FILENAME) . '.html';
+        if (Storage::exists($reportPath)) {
+            $currentContent = Storage::get($reportPath);
+            $newContent = "<tr><td>" . now() . "</td><td>" . $description . "</td></tr>";
+            $updatedContent = str_replace('</table>', $newContent . '</table>', $currentContent);
+            Storage::put($reportPath, $updatedContent);
         }
     }
 }
