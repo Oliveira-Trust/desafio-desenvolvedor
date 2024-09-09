7433cde789f32ca4e16e1b076a8baa6b55c73166
Rodrigo Medeiros Grassioto <rodrigomgrassioto@gmail.com>
2024-09-09 11:33:33 -0300

    Test: testar formato data de RptDt

--- app/Imports/FileImport.php
+++ app/Imports/FileImport.php
@@ -78,7 +78,7 @@ class FileImport implements ToModel, WithHeadingRow, WithChunkReading, WithBatch
             $dateObject = \DateTime::createFromFormat($format, $date);
 
             if ($dateObject && $dateObject->format($format) === $date) {
-                return $dateObject->format('Y-m-d');
+                dd($dateObject->format('Y-m-d'));
             }
         }
 
