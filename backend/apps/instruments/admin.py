db2c777089b455de8fb20e7c9eaf61e48034d7b0
Marcelo Barbosa <marcelojorgea@gmail.com>
2024-10-03 22:17:02 -0300

    feat Adiciona modelos Instrument e InstrumentFile ao admin

--- backend/apps/instruments/admin.py
+++ backend/apps/instruments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from apps.instruments.models import Instrument, InstrumentFile
+
+admin.site.register(InstrumentFile)
+admin.site.register(Instrument)
+
 # Register your models here.
