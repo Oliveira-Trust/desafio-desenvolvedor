f462b9ddbcc9c977a2a175ff6e5101792664d65e
Marcelo Barbosa <marcelojorgea@gmail.com>
2024-10-03 22:16:38 -0300

    feat adiciona biblioteca de filtros, jwt, flake8, isort, configurações e adiciona media nas urls do core

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,9 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('apps.instruments.urls')),
+    path('', include('apps.instruments.urls')),
     path('django-rq/', include('django_rq.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
