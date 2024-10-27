e32c667343ebb9ce3edf9d7269ace8c7a293c849
fspjonny <fabiosilvapedro@yahoo.com.br>
2024-10-27 00:33:29 -0300

    Main modificado para iniciar bancos de dados, 2ºdia

--- proj_api/app/main.py
+++ proj_api/app/main.py
@@ -1,12 +1,28 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
+from app.database import initialize_databases
 from app.routes import router
 
-app = FastAPI()
 
-# Página de boavinda para não apresentar nada vazio.
-@app.get("/", include_in_schema=False)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Inicializa os bancos e coleções antes de iniciar a aplicação
+    # para ter certeza que os bancos estão prontos para serem usados.
+    await initialize_databases()
+
+    yield  # Mantém a aplicação ativa enquanto o servidor está rodando
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+# Mensagem de boas-vindas para a raiz da API.
+# Não é necessário, mas é apenas para orientar o usuário.
+@app.get('/', include_in_schema=False)
 def root():
-    return {"message": "Bem-vindo ao desafio do Desenvolvedor API!"}
+    return {'message': 'Bem-vindo ao desafio do Desenvolvedor API!'}
 
-# Inclusão das rotas.
+
 app.include_router(router)
