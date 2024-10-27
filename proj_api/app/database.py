859ac21754b22315f345de6537a42a3fbddf8dfd
fspjonny <fabiosilvapedro@yahoo.com.br>
2024-10-27 00:02:17 -0300

    Implementado bancos de dados para tratar os arquivos de dados.

--- proj_api/app/database.py
+++ proj_api/app/database.py
@@ -1,8 +1,25 @@
-# app/database.py
 from motor.motor_asyncio import AsyncIOMotorClient
-from fastapi import FastAPI
 
-DATABASE_URL = "mongodb://mongodb_container:27017/upload_db"
-client = AsyncIOMotorClient(DATABASE_URL)
-# Nome do banco de dados
-database = client.upload_db
+# Conexão com MongoDB
+MONGODB_URL = 'mongodb://mongodb_container:27017'
+client = AsyncIOMotorClient(MONGODB_URL)
+
+# Bancos e coleções
+historico_db = client['historico']
+datalake_db = client['datalake']
+
+# Coleções específicas
+historico_collection = historico_db['uploads']
+datalake_collection = datalake_db['dados']
+
+
+# Função para inicializar bancos e coleções
+async def initialize_databases():
+    # Cria um documento temporário se a coleção estiver vazia, depois remove
+    if not await historico_collection.count_documents({}):
+        await historico_collection.insert_one({'init': 'init'})
+        await historico_collection.delete_one({'init': 'init'})
+
+    if not await datalake_collection.count_documents({}):
+        await datalake_collection.insert_one({'init': 'init'})
+        await datalake_collection.delete_one({'init': 'init'})
