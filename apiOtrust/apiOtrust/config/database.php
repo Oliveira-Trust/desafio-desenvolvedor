2d084ebc0158e2e18902ec38be8952c8c7d48e64
Alan Santana <72580528+gankun26@users.noreply.github.com>
2025-01-20 19:22:44 -0300

    feat: configurando database

--- apiOtrust/apiOtrust/config/database.php
+++ apiOtrust/apiOtrust/config/database.php
@@ -112,6 +112,19 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('DB_HOST', 'mongodb://localhost:27017/'),
+            'port'     => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'apiOtrust'),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'options'  => [
+            'database' => env('DB_AUTH_DATABASE', 'admin'), // banco de dados de autenticação
+            ],
+        ],
+
+
     ],
 
     /*
