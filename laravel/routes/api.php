10ab0f089a8d4bf77eaea0c9ac75b442caf0365d
matheus-arj <matheus.arj@hotmail.com>
2025-05-13 00:36:35 -0300

    feat: created api to search records

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\UploadController;
 
 Route::post('/upload', [UploadController::class, 'upload']);
 Route::get('/history', [UploadController::class, 'history']);
+Route::get('/search', [UploadController::class, 'search']);
