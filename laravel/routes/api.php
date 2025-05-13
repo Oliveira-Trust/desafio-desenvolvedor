e3bfa2344421383c6b2ec54cb72ea4e94f0cb90b
matheus-arj <matheus.arj@hotmail.com>
2025-05-13 00:23:40 -0300

    feat: created history api

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UploadController;
 
 Route::post('/upload', [UploadController::class, 'upload']);
-
+Route::get('/history', [UploadController::class, 'history']);
