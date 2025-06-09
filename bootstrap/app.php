265535154120519c3b1de20c3e3869b3b5221154
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-09 18:10:34 -0300

    chore: add Sanctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            $routesFile = base_path('routes/v1.php');
+
+            Route::middleware('api')
+                ->prefix('v1')
+                ->group($routesFile);
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
