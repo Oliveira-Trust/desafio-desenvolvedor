4d578944804b0e92959befb5c5690e2a5ed93480
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-09 18:54:38 -0300

    chore: create Presentation and Shared layers namespaces

--- app/Domain/User/Models/User.php
+++ app/Domain/User/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace Domain\User\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
