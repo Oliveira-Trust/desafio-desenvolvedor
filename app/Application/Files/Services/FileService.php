0eae0a888127277389d74b3d064d92c67891960f
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-10 20:58:19 -0300

    feat: create file service

---
+++ app/Application/Files/Services/FileService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Application\Files\Services;
+
+use Application\Files\Jobs\ProcessConsolidatedFile;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Support\Facades\Storage;
+
+class FileService {
+    private string $disk;
+    private Filesystem $storage;
+
+    public function __construct()
+    {
+        $this->disk = 's3';
+        $this->storage = Storage::disk($this->disk);
+    }
+
+    public function setDisk(string $driver) {
+        $this->disk = $driver;
+        return $this;
+    }
+
+    public function exists($path): bool {
+        return $this->storage->exists($path);
+    }
+
+    public function store($path, $file) {
+        $this->storage->put($path, $file);
+    }
+
+    public function sendToQueue($path) {
+        ProcessConsolidatedFile::dispatch($path);
+    }
+}
