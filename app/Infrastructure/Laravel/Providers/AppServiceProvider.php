806516acab1accc6fca078cbe60e46662aacbe57
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-09 21:39:41 -0300

    chore: create Infrastructure layer

--- app/Infrastructure/Laravel/Providers/AppServiceProvider.php
+++ app/Infrastructure/Laravel/Providers/AppServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace Infrastructure\Laravel\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
+    \Infrastructure\Laravel\Providers\AppServiceProvider::class,
 ];
