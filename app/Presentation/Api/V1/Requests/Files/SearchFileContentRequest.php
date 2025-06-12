35545fc481011f4216133360c655bb8f97545f55
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-12 15:31:35 -0300

    feat: create search file content request

---
+++ app/Presentation/Api/V1/Requests/Files/SearchFileContentRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Presentation\Api\V1\Requests\Files;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchFileContentRequest extends FormRequest {
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'attr' => ['required', 'string', 'min:3', 'max:30'],
+        ];
+    }
+}
