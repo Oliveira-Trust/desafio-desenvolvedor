122c935759cc2996e3ea70151d353ee19b63dfc0
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-12 15:14:11 -0300

    feat: add getHistory on ConsolidatedFileRepository

--- app/Domain/Files/Repositories/ConsolidatedFileRepository.php
+++ app/Domain/Files/Repositories/ConsolidatedFileRepository.php
@@ -5,6 +5,7 @@ namespace Domain\Files\Repositories;
 use Domain\Files\Entities\ConsolidatedFile;
 
 interface ConsolidatedFileRepository {
+    public function getHistory(string|null $filename, string|null $date);
     public function createNew(ConsolidatedFile $data);
     public function registerLine(string $filename, array $chunk);
 }
