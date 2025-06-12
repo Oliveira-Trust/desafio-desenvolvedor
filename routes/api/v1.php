d6f9694aecbedbec778974dba71d1dca6f7a1416
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-12 19:54:46 -0300

    feat: create auth router group and add login route

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,13 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Presentation\Api\V1\Controllers\FileController;
+use Presentation\Api\V1\Controllers\{FileController, AuthController};
 
 Route::get('/', function () {
     return response()->json(['message' => 'welcome to file manager api']);
 });
 
-Route::prefix('files')->group(function () {
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::prefix('files')->middleware('auth:sanctum')->group(function () {
     Route::post('/', [FileController::class, 'storeFile']);
     Route::get('/', [FileController::class, 'getHistory']);
     Route::get('/{filename}', [FileController::class, 'searchContent']);
