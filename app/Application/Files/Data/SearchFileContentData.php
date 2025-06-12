511a36af41fbae139b75b3efbb70fbca45e51801
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-12 16:15:29 -0300

    refactor: rename filename attribute

--- app/Application/Files/Data/SearchFileContentData.php
+++ app/Application/Files/Data/SearchFileContentData.php
@@ -5,6 +5,6 @@ namespace Application\Files\Data;
 use Spatie\LaravelData\Data;
 
 class SearchFileContentData extends Data {
-    public function __construct(public string $filenameOrId, public string $s)
+    public function __construct(public string $filename, public string $s)
     {}
 }
