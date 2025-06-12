754cb95c4d480e02b019bb86f940f2012d16ac4a
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-11 23:09:33 -0300

    refactor: add read stream method

--- app/Application/Files/Services/FileService.php
+++ app/Application/Files/Services/FileService.php
@@ -4,16 +4,25 @@ namespace Application\Files\Services;
 
 use Application\Files\Jobs\ProcessConsolidatedFile;
 use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 
 class FileService {
     private string $disk;
+
     private Filesystem $storage;
+
+    private string $basePath;
 
     public function __construct()
     {
         $this->disk = 's3';
+        $this->basePath = config('upload.basePath');
         $this->storage = Storage::disk($this->disk);
+    }
+
+    private function resolvePath($path): string {
+        return implode('/', [$this->basePath, $path]);
     }
 
     public function setDisk(string $driver) {
@@ -21,19 +30,31 @@ class FileService {
         return $this;
     }
 
-    public function exists($path): bool {
+    public function exists(string $path): bool {
         return $this->storage->exists($path);
     }
 
-    public function store($path, $file) {
-        $this->storage->put($path, $file);
+    public function store(string $filename, UploadedFile $file): string|bool {
+        $path = $this->resolvePath($filename);
+
+        $stored = $this->storage->put(
+            $this->resolvePath($filename),
+            file_get_contents($file),
+        );
+
+        if ($stored) return $path;
+        return false;
     }
 
-    public function get($path): string {
-        return $this->storage->get($path);
+    public function get(string $path): string {
+        return $this->storage->get($this->resolvePath($path));
     }
 
-    public function sendToQueue($path) {
-        ProcessConsolidatedFile::dispatch($path);
+    public function readStream(string $path) {
+        return $this->storage->readStream($this->resolvePath($path));
+    }
+
+    public function sendToQueue(string $path) {
+        ProcessConsolidatedFile::dispatch($this->resolvePath($path));
     }
 }
