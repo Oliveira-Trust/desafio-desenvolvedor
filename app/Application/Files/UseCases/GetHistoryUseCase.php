6848872acb069403ff6c4331165241364199bef2
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-12 15:12:51 -0300

    feat: implement get all file history usecase

--- app/Application/Files/UseCases/GetHistoryUseCase.php
+++ app/Application/Files/UseCases/GetHistoryUseCase.php
@@ -2,11 +2,19 @@
 
 namespace Application\Files\UseCases;
 
+use Infrastructure\Laravel\Database\Repositories\MongoDBConsolidatedFileRepository;
+use Presentation\Api\V1\Resources\Files\GetFileHistoryResource;
 use Shared\Interfaces\UseCase;
 
 final class GetHistoryUseCase implements UseCase {
+    public function __construct(
+        private MongoDBConsolidatedFileRepository $repository,
+    )
+    {}
+
     public function execute($data)
     {
-        //
+        $files = $this->repository->getHistory($data->filename, $data->date);
+        return GetFileHistoryResource::collection($files);
     }
 }
