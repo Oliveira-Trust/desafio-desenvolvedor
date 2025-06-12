206a2579bb40be940112bfdb0b155643bbc2f2b7
Francisco Damião Terto <francisco.tsilva@slserv.com.br>
2025-06-11 23:10:58 -0300

    feat: create consolidated file repository interface

---
+++ app/Domain/Files/Repositories/ConsolidatedFileRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Domain\Files\Repositories;
+
+use Domain\Files\Entities\ConsolidatedFile;
+
+interface ConsolidatedFileRepository {
+    public function createNew(ConsolidatedFile $data);
+    public function registerLine(string $filename, array $chunk);
+}
