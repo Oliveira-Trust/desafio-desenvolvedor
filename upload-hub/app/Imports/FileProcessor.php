efdb225d98e2097e4927f6afad1accf3be3e085f
Cintia Ribeiro <cintiarbrandao@gmail.com>
2025-08-28 05:06:30 -0300

    refactor: Organiza code processo de importação

--- upload-hub/app/Imports/FileProcessor.php
+++ upload-hub/app/Imports/FileProcessor.php
@@ -2,21 +2,16 @@
 
 namespace App\Imports;
 
-ini_set('memory_limit', '2G');
-
 use Maatwebsite\Excel\Row;
-use App\Models\ImportDataFile;
-use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\OnEachRow;
 use App\Repositories\ImportDataFileRepository;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 
-class FileProcessor implements OnEachRow, WithChunkReading, WithHeadingRow
+class FileProcessor implements OnEachRow, WithHeadingRow
 {
     private int $totalLines = 0;
     private int $processedLines = 0;
-    private array $batch = [];
 
     public function __construct(
         private ImportDataFileRepository $importDataFileRepository,
@@ -40,14 +35,12 @@ class FileProcessor implements OnEachRow, WithChunkReading, WithHeadingRow
 
         $this->processedLines++;       
         
-        
     }
 
     public function headingRow(): int
     {
         return 2; // segunda linha como cabeçalho
     }
-    
 
     public function getTotalLines(): int
     {
